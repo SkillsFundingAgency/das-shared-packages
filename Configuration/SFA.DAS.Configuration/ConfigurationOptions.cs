8309f08866a2cdeb75b00d0b78e461a1ebec17c1
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-06-08 09:56:26 +0100

    Correct merge problem

--- Configuration/SFA.DAS.Configuration/ConfigurationOptions.cs
+++ Configuration/SFA.DAS.Configuration/ConfigurationOptions.cs
@@ -1,4 +1,5 @@
-﻿namespace SFA.DAS.Configuration
+﻿
+namespace SFA.DAS.Configuration
 {
     public class ConfigurationOptions
     {
@@ -12,36 +13,6 @@
         public string ServiceName { get; private set; }
         public string EnvironmentName { get; private set; }
         public string VersionNumber { get; private set; }
-
-        private string GetServiceName()
-        {
-            return GetAssemblyName().Name;
-        }
-        private string GetEnvironmentName()
-        {
-            var environmentName = Environment.GetEnvironmentVariable("DASENV");
-            return string.IsNullOrEmpty(environmentName) ? "Dev" : environmentName;
-        }
-        private string GetVersionNumer()
-        {
-            var version = GetAssemblyName().Version;
-            return $"{version.Major}.{version.Minor}";
-        }
-        private AssemblyName GetAssemblyName()
-        {
-            var assembly = Assembly.GetEntryAssembly();
-            if (assembly == null)
-            {
-                throw new InvalidOperationException("Unable to determine assembly");
-            }
-
-            var assemblyName = assembly.GetName();
-            if (assemblyName == null)
-            {
-                throw new InvalidOperationException("Unable to determine assembly name");
-            }
-
-            return assemblyName;
-        }
+        
     }
 }
