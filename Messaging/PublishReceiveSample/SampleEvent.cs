992759930cae0ea081e16c828b25b1ca0a404e14
Simon Bull <simon.bull@taurumconsulting.com>
2016-07-08 09:57:40 +0100

    Refactor messaging interfaces and file system implementation. No refactored Azure Service Bus implementation yet.

--- Messaging/PublishReceiveSample/SampleEvent.cs
+++ Messaging/PublishReceiveSample/SampleEvent.cs
@@ -4,6 +4,11 @@ namespace PublishReceiveSample
 {
     public class SampleEvent
     {
+        public SampleEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+            Timestamp = DateTimeOffset.Now;
+        }
         public DateTimeOffset Timestamp { get; set; }
         public string Id { get; set; }
     }
---
+++ Messaging/SFA.DAS.Messaging/FileSystem/FileSystemMessageService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Messaging.FileSystem
+{
+    public class FileSystemMessageService : IMessagePublisher, IPollingMessageReceiver
+    {
+        private readonly string _storageDirectory;
+
+        public FileSystemMessageService(string storageDirectory)
+        {
+            _storageDirectory = storageDirectory;
+            if (!Directory.Exists(_storageDirectory))
+            {
+                Directory.CreateDirectory(_storageDirectory);
+            }
+        }
+
+
+        public async Task PublishAsync(object message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+
+            if (!Directory.Exists(_storageDirectory))
+            {
+                Directory.CreateDirectory(_storageDirectory);
+            }
+
+            var path = Path.Combine(_storageDirectory, Guid.NewGuid() + ".json");
+            using (var stream = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            using (var writer = new StreamWriter(stream))
+            {
+                await writer.WriteAsync(json);
+                writer.Close();
+            }
+        }
+
+        public async Task<Message<T>> ReceiveAsAsync<T>() where T : new()
+        {
+            var directory = new DirectoryInfo(_storageDirectory);
+            if (!directory.Exists)
+            {
+                return null;
+            }
+
+            var jsonFiles = directory.GetFiles("*.json");
+            var lockFiles = directory.GetFiles("*.lck");
+            var nextFile = jsonFiles
+                .Where(jf => !lockFiles.Any(lf => lf.Name.StartsWith(jf.Name)))
+                .OrderByDescending(jf => jf.LastWriteTimeUtc)
+                .FirstOrDefault();
+            if (nextFile == null)
+            {
+                return null;
+            }
+
+            return await FileSystemMessage<T>.Lock(nextFile);
+        }
+    }
+}
