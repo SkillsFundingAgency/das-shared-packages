b997b3075089bfaf49cbfbf14e983e4bb7b9b9ad
Simon Bull <simon.bull@taurumconsulting.com>
2016-07-19 16:44:59 +0100

    Add storage queue implementation

---
+++ Messaging/SFA.DAS.Messaging.AzureStorageQueue/AzureStorageQueueMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Messaging.AzureStorageQueue
+{
+    public class AzureStorageQueueMessage<T> : Message<T>
+    {
+        private readonly CloudQueueMessage _message;
+        private readonly CloudQueue _queue;
+
+        public AzureStorageQueueMessage(CloudQueueMessage message, CloudQueue queue)
+        {
+            _message = message;
+            _queue = queue;
+            Content = JsonConvert.DeserializeObject<T>(message.AsString);
+        }
+
+        public override async Task CompleteAsync()
+        {
+            await _queue.DeleteMessageAsync(_message);
+        }
+
+        public override Task AbortAsync()
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ Messaging/SFA.DAS.Messaging.AzureStorageQueue/AzureStorageQueueService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Messaging.AzureStorageQueue
+{
+    public class AzureStorageQueueService : IMessagePublisher, IPollingMessageReceiver
+    {
+        private readonly string _connectionString;
+        private readonly string _queueName;
+
+        public AzureStorageQueueService(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+            _queueName = queueName;
+        }
+
+        public async Task PublishAsync(object message)
+        {
+            var content = JsonConvert.SerializeObject(message);
+
+            var queue = GetQueue();
+            await queue.AddMessageAsync(new CloudQueueMessage(content));
+        }
+
+        public async Task<Message<T>> ReceiveAsAsync<T>() where T : new()
+        {
+            var queue = GetQueue();
+            var message = await queue.GetMessageAsync();
+            if (message == null)
+            {
+                return null;
+            }
+
+            return new AzureStorageQueueMessage<T>(message, queue);
+        }
+
+        private CloudQueue GetQueue()
+        {
+
+            var storageAccount = CloudStorageAccount.Parse(_connectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            return queueClient.GetQueueReference(_queueName);
+        }
+    }
+}
