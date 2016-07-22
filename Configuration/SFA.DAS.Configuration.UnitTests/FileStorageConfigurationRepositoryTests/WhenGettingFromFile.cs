066d9b8241b394c34de948698d3bef8b704f5811
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-22 11:42:32 +0100

    Updated the way that the app_data folder is read for console apps. Added test around this to make sure it reads the file as expected

---
+++ Configuration/SFA.DAS.Configuration.UnitTests/FileStorageConfigurationRepositoryTests/WhenGettingFromFile.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using SFA.DAS.Configuration.FileStorage;
+
+namespace SFA.DAS.Configuration.UnitTests.FileStorageConfigurationRepositoryTests
+{
+    public class WhenGettingFromFile
+    {
+        private FileStorageConfigurationRepository _configurationRepository;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _configurationRepository = new FileStorageConfigurationRepository();
+        }
+
+        [Test]
+        public void ThenTheFileIsCorrectlyReadfromTheAppDataFolder()
+        {
+            //Act
+            var config = _configurationRepository.Get("test", "test", "1.0");
+
+            //Assert
+            Assert.IsNotNull(config);
+        }
+    }
+}
