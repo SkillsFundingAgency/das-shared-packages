2e2edbf0f4b3ef736d681e2e9af1d2e6c587217b
ijrussell <i.j.russell@tesco.net>
2016-10-05 10:24:41 +0100

    Version 1.2.0.x

---
+++ SFA.DAS.Tasks.Api/SFA.DAS.Tasks.Api.Client/Templates/CreateCommitmentTemplate.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Tasks.Api.Client.Templates
+{
+    public class CreateCommitmentTemplate
+    {
+        public const long TemplateId = 1;
+
+        public long CommitmentId { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ SFA.DAS.Tasks.Api/SFA.DAS.Tasks.Api.Client/Templates/SubmitCommitmentTemplate.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Tasks.Api.Client.Templates
+{
+    public class SubmitCommitmentTemplate
+    {
+        public const long TemplateId = 2;
+
+        public long CommitmentId { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+    }
+}
