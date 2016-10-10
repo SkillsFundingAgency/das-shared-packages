a07dc2d7b31c2847b1d8f4b888ffc28e5993cc4a
ijrussell <i.j.russell@tesco.net>
2016-10-10 14:57:23 +0100

    Added email class and api

--- SFA.DAS.Notifications.Api/SFA.DAS.Notifications.Api.Client/INotificationsApi.cs
+++ SFA.DAS.Notifications.Api/SFA.DAS.Notifications.Api.Client/INotificationsApi.cs
@@ -1,7 +1,10 @@
-﻿namespace SFA.DAS.Notifications.Api.Client
+﻿using System.Threading.Tasks;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.Notifications.Api.Client
 {
     public interface INotificationsApi
     {
-        
+        Task SendEmail(Email email);
     }
 }
