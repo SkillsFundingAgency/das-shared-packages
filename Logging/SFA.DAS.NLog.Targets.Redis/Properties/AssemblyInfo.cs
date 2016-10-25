753e6ddf83269678152f45b34520442cf3d74349
Chris Fröberg <froberg@users.noreply.github.com>
2016-10-25 16:39:48 +0100

    Redis logging (#12)
    * Added Redis Nlog target
    
    * Removed file
    
    * Added readme and nuspec file
    
    * Lowered version of NLog, added timestamp
    
    * [nlog-redis-target] Logging just level name and not the object
    
    * [nlog-target] Renamed solution

---
+++ Logging/SFA.DAS.NLog.Targets.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SFA.DAS.NLog.Targets.Redis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFA.DAS.NLog.Targets.Redis")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ce1c3fe-ee94-4e5d-bf7e-a5c7eb05ee1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
