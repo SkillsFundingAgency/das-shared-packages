d4107963ac0ed041b3ca0b44dc63e0e533a936ee
ijrussell <i.j.russell@tesco.net>
2016-11-08 09:43:50 +0000

    Added dateofbirth, ni number, employer ref and provider ref properties

--- SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/Apprenticeship.cs
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/Apprenticeship.cs
@@ -8,6 +8,8 @@ namespace SFA.DAS.Commitments.Api.Types
         public long CommitmentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string NINumber { get; set; }
         public string ULN { get; set; }
         public TrainingType TrainingType { get; set; }
         public string TrainingCode { get; set; }
@@ -17,6 +19,8 @@ namespace SFA.DAS.Commitments.Api.Types
         public DateTime? EndDate { get; set; }
         public ApprenticeshipStatus Status { get; set; }
         public AgreementStatus AgreementStatus { get; set; }
+        public string EmployerRef { get; set; }
+        public string ProviderRef { get; set; }
 
         public string ApprenticeshipName => $"{FirstName} {LastName}";
     }
