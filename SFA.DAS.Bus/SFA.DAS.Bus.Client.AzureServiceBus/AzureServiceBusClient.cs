c0595e859edd0a9dc127e5a98345f757fbd2b29c
Michael Young <mick.young1981@googlemail.com>
2017-01-06 15:24:27 +0000

    Basic implementation of Azure Service Bus client.
    Basic implementation of Azure Service Bus client.

---
+++ SFA.DAS.Bus/SFA.DAS.Bus.Client.AzureServiceBus/AzureServiceBusClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SFA.DAS.Bus.Client.AzureServiceBus
+{
+    public class AzureServiceBusClient : IBusClient
+    {
+        private readonly string _connectionString;
+        private readonly string _queueName;
+
+        public AzureServiceBusClient(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+            _queueName = queueName;
+        }
+
+        public async Task PublishAsync(object message)
+        {
+            var client = QueueClient.CreateFromConnectionString(_connectionString, _queueName);
+            var brokeredMessage = new BrokeredMessage(message)
+            {
+                MessageId = Guid.NewGuid().ToString()
+            };
+
+            await client.SendAsync(brokeredMessage);
+        }
+    }
+}
---
+++ SFA.DAS.Bus/SFA.DAS.Bus.Client/IBusClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.Bus.Client
+{
+    public interface IBusClient
+    {
+        Task PublishAsync(object message);
+    }
+}
