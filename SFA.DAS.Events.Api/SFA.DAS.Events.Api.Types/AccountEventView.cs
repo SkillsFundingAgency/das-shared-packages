02c7d606f6bfd45cbc48713a7bf481115652b100
Michael Young <mick.young1981@googlemail.com>
2017-01-11 15:21:29 +0000

    Added types required for account events.

---
+++ SFA.DAS.Events.Api/SFA.DAS.Events.Api.Types/AccountEventView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.Events.Api.Types
+{
+    public class AccountEventView
+    {
+        public long Id { get; set; }
+        public string Event { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string DasAccountId { get; set; }
+    }
+}
