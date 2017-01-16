bf9a756e1bc89b70e6a1b0b209f654f77228cf48
Michael Young <mick.young1981@googlemail.com>
2017-01-16 08:42:37 +0000

    Added agreement event unit tests.

---
+++ SFA.DAS.Events.Api/SFA.DAS.Events.Api.Client.UnitTests/EventsApiAgreementsTests/WhenIGetAgreementEventsByDateRange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.Events.Api.Types;
+
+namespace SFA.DAS.Events.Api.Client.UnitTests.EventsApiAgreementsTests
+{
+    [TestFixture]
+    public class WhenIGetAgreementEventsByDateRange : EventsApiTestBase
+    {
+        [Test]
+        public async Task ThenAgreementEventsAreReturned()
+        {
+            var fromDate = DateTime.Now.AddDays(-30);
+            var toDate = DateTime.Now;
+            var pageSize = 500;
+            var pageNumber = 3;
+
+            var url = $"{BaseUrl}api/events/engagements?fromDate={fromDate:yyyyMMddHHmmss}&toDate={toDate:yyyyMMddHHmmss}&pageSize={pageSize}&pageNumber={pageNumber}";
+            
+            var expectedEvents = new List<AgreementEventView> { new AgreementEventView { CreatedOn = DateTime.Now.AddDays(-1), EmployerAccountId = "ABC123", Event = "Test", Id = 87435, ProviderId = "ZZZ999" } };
+
+            SecureHttpClient.Setup(x => x.GetAsync(url, ClientToken)).ReturnsAsync(JsonConvert.SerializeObject(expectedEvents));
+
+            var response = await Api.GetAgreementEventsByDateRange(fromDate, toDate, pageSize, pageNumber);
+
+            response.ShouldBeEquivalentTo(expectedEvents);
+        }
+    }
+}
