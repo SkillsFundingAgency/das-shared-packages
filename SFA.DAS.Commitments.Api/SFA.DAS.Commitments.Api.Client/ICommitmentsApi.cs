35d0eb94e703efd3ac7943d7c9b33fe279d97c9c
Mark Gwilliam <github@markgwilliam.com>
2017-01-26 20:18:33 +0000

    added delete operations to commitments api client

--- SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Client/ICommitmentsApi.cs
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Client/ICommitmentsApi.cs
@@ -15,6 +15,8 @@ namespace SFA.DAS.Commitments.Api.Client
         Task UpdateEmployerApprenticeship(long employerAccountId, long commitmentId, long apprenticeshipId, Apprenticeship apprenticeship);
         Task PatchEmployerApprenticeship(long employerAccountId, long commitmentId, long apprenticeshipId, PaymentStatus paymentStatus);
         Task CreateEmployerApprenticeship(long employerAccountId, long commitmentId, Apprenticeship apprenticeship);
+        Task DeleteEmployerApprenticeship(long employerAccountId, long apprenticeshipId);
+        Task DeleteEmployerCommitment(long employerAccountId, long commitmentId);
 
         Task<List<CommitmentListItem>> GetProviderCommitments(long providerId);
         Task<Commitment> GetProviderCommitment(long providerId, long commitmentId);
@@ -23,5 +25,7 @@ namespace SFA.DAS.Commitments.Api.Client
         Task UpdateProviderApprenticeship(long providerId, long commitmentId, long apprenticeshipId, Apprenticeship apprenticeship);
         Task PatchProviderCommitment(long providerId, long commitmentId, LastAction lastAction);
         Task BulkUploadApprenticeships(long providerId, long commitmentId, IList<Apprenticeship> apprenticeships);
+        Task DeleteProviderApprenticeship(long providerId, long apprenticeshipId);
+        Task DeleteProviderCommitment(long providerId, long commitmentId);
     }
 }
