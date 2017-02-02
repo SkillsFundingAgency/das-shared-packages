5e04c4e05f55de51586e61a24b42d4c8a064c20e
Lee Wadhams <lee.wadhams@valtech.com>
2017-02-02 14:19:51 +0000

    Update Commitment Patch for Employer and Provider
    Takes the name and email of the submitter when Patching Commitment
    
    Adds Employer and Provider properties onto Commitment with the last
    updater.

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/CommitmentSubmission.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Commitments.Api.Types
+{
+    public sealed class CommitmentSubmission
+    {
+        public LastAction Action { get; set; }
+        public LastUpdateInfo LastUpdatedByInfo { get; set; }
+    }
+}
