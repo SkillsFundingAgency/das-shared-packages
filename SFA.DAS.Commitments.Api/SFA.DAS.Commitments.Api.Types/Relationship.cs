afec3444d89274c81929342e6b79c146efbf49fa
Christopher Foster <Christopher Foster>
2017-02-24 10:05:20 +0000

    Adds Relationship to Api types

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/Relationship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Commitments.Api.Types
+{
+    public class Relationship
+    {
+        public long Id { get; set; }
+        public long EmployerAccountId { get; set; }
+        public string LegalEntityId { get; set; }
+        public string LegalEntityName { get; set; }
+        public long ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public bool Verified { get; set; }
+    }
+}
