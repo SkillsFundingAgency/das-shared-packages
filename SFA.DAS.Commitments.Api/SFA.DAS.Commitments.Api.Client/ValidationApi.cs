17947954cf4d4ad82c0f0191e128bc91f230e296
froberg <christofer.froberg@gmail.com>
2017-03-13 12:04:26 +0100

    Added client for validation

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Client/ValidationApi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using SFA.DAS.Commitments.Api.Client.Configuration;
+using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.Commitments.Api.Types.Validation;
+
+namespace SFA.DAS.Commitments.Api.Client
+{
+    public class ValidationApi : HttpClientBase, IValidationApi
+    {
+        private readonly ICommitmentsApiClientConfiguration _configuration;
+
+        public ValidationApi(ICommitmentsApiClientConfiguration configuration)
+            : base(configuration.ClientToken)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+            _configuration = configuration;
+        }
+
+        public Task<ApprenticeshipOverlapValidationResult> ValidateOverlapping(ApprenticeshipOverlapValidationRequest request)
+        {
+            var url = $"{_configuration.BaseUrl}api/validation/apprenticeship";
+            return GetValidation(url, request);
+        }
+
+        public Task<IEnumerable<ApprenticeshipOverlapValidationResult>> ValidateOverlapping(IEnumerable<ApprenticeshipOverlapValidationRequest> requests)
+        {
+            var url = $"{_configuration.BaseUrl}api/validation/apprenticeship";
+            return GetValidation(url, requests);
+        }
+
+        private async Task<ApprenticeshipOverlapValidationResult> GetValidation(string url, ApprenticeshipOverlapValidationRequest request)
+        {
+            var data = JsonConvert.SerializeObject(request);
+            var result = await PostAsync(url, data);
+            return JsonConvert.DeserializeObject<ApprenticeshipOverlapValidationResult>(result);
+        }
+
+        private async Task<IEnumerable<ApprenticeshipOverlapValidationResult>> GetValidation(string url, IEnumerable<ApprenticeshipOverlapValidationRequest> requests)
+        {
+            var data = JsonConvert.SerializeObject(requests);
+            var result = await PostAsync(url, data);
+            return JsonConvert.DeserializeObject<IEnumerable<ApprenticeshipOverlapValidationResult>>(result);
+        }
+    }
+}
