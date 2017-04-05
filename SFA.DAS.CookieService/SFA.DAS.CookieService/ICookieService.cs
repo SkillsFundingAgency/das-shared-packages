67a1065add4ed6ad96f4188ba78effc44985c112
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-05 07:56:56 +0100

    Initial project creation
    Created project for CookieService and added nuspec file. This allows
    encrypted storage of information in a named cookie which can be created,
    updated and deleted.

---
+++ SFA.DAS.CookieService/SFA.DAS.CookieService/ICookieService.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace SFA.DAS.CookieService
+{
+    public interface ICookieService<T>
+    {
+        void Create(HttpContextBase context, string name, T content, int expireDays);
+        void Update(HttpContextBase context, string name, T content);
+        void Delete(HttpContextBase context, string name);
+        T Get(HttpContextBase context, string name);
+    }
+}
