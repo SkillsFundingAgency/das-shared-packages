a6805a8fa8802b47d27f68dcab8e8b6f5098be00
Michael Young <mick.young1981@googlemail.com>
2017-04-13 09:49:32 +0100

    Made start and end date nullable.

--- SFA.DAS.Events.Api/SFA.DAS.Events.Api.Types/ApprenticeshipEventView.cs
+++ SFA.DAS.Events.Api/SFA.DAS.Events.Api.Types/ApprenticeshipEventView.cs
@@ -15,8 +15,8 @@ namespace SFA.DAS.Events.Api.Types
         public string EmployerAccountId { get; set; }
         public TrainingTypes TrainingType { get; set; }
         public string TrainingId { get; set; }
-        public DateTime TrainingStartDate { get; set; }
-        public DateTime TrainingEndDate { get; set; }
+        public DateTime? TrainingStartDate { get; set; }
+        public DateTime? TrainingEndDate { get; set; }
         public decimal? TrainingTotalCost { get; set; }
         public int PaymentOrder { get; set; }
         public string LegalEntityId { get; set; }
