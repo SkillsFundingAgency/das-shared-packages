a895b6cbf81d9da02ca587fd2e98b2ad48c3d5cb
Michael Young <mick.young1981@googlemail.com>
2017-05-03 09:05:52 +0100

    Added last updated info to all commands.

--- SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/DeleteRequest.cs
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/DeleteRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace SFA.DAS.Commitments.Api.Types
+﻿using SFA.DAS.Commitments.Api.Types.Commitment.Types;
+
+namespace SFA.DAS.Commitments.Api.Types
 {
     public class DeleteRequest
     {
         public string UserId { get; set; }
+        public LastUpdateInfo LastUpdatedByInfo { get; set; }
     }
 }
