74ac31314102f36dcdb8f5770f2a541de8d436bd
froberg <christofer.froberg@gmail.com>
2017-05-23 13:40:47 +0100

    Added type and methods

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/BulkUploadFileRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Commitments.Api.Types
+{
+    public class BulkUploadFileRequest
+    {
+        public long CommitmentId { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Data { get; set; }
+    }
+}
