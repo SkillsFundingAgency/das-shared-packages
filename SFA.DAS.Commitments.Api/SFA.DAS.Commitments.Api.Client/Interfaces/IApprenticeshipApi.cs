07150e31b188269895c4ab696ae2c64bd780c2b5
froberg <christofer.froberg@gmail.com>
2017-06-08 13:39:39 +0100

    Added apprenticeship API with get price history

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Client/Interfaces/IApprenticeshipApi.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+using SFA.DAS.Commitments.Api.Types.Apprenticeship;
+
+namespace SFA.DAS.Commitments.Api.Client.Interfaces
+{
+    public interface IApprenticeshipApi
+    {
+        Task<PriceHistory> GetPriceHistory(long apprenticeshipId);
+    }
+}
