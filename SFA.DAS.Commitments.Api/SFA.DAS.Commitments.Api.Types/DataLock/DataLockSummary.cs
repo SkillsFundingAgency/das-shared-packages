eca0448df125e553f27d8a4b5d74cee9c86b2729
froberg <christofer.froberg@gmail.com>
2017-06-12 14:34:26 +0100

    Added types and patch for DL

---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/DataLock/DataLockSummary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Commitments.Api.Types.DataLock
+{
+    public class DataLockSummary
+    {
+        public IEnumerable<DataLockStatus> DataLockWithCourseMismatch { get; set; }
+
+        public IEnumerable<DataLockStatus> DataLockWithOnlyPriceMismatch { get; set; }
+    }
+}
---
+++ SFA.DAS.Commitments.Api/SFA.DAS.Commitments.Api.Types/DataLock/Types/DataLockUpdateType.cs
@@ -0,0 +1,8 @@
+namespace SFA.DAS.Commitments.Api.Types.DataLock.Types
+{
+    public enum DataLockUpdateType
+    {
+        ApproceChanges = 1,
+        RejectChanges = 2
+    }
+}
