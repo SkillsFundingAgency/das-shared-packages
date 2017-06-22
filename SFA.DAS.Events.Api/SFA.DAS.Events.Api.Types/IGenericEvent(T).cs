d8fab6daf6ff7bc898466babf5256b84a0f28ab7
Michael Young <mick.young1981@googlemail.com>
2017-06-22 10:30:29 +0100

    Added GenericEvent(T) Interface

---
+++ SFA.DAS.Events.Api/SFA.DAS.Events.Api.Types/IGenericEvent(T).cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SFA.DAS.Events.Api.Types
+{
+    public interface IGenericEvent<T> : IEventView
+    {
+        DateTime CreatedOn { get; set; }
+        T Payload { get; set; }
+        string ResourceUri { get; set; }
+        string ResourceType { get; set; }
+        string ResourceId { get; set; }
+    }
+}
