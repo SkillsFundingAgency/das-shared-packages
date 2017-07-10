c6781b22a160dc75b31184091539eecb14b4ed12
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-07-10 16:09:57 +0100

    Add Base client and HttpClient builder

---
+++ HttpClient/SFA.DAS.SecureHttpClient/ApiClientBase.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SFA.DAS.Http
+{
+    public abstract class ApiClientBase
+    {
+        private readonly QueryStringHelper _queryStringHelper;
+        private readonly HttpClient _client;
+
+        protected ApiClientBase(HttpClient client)
+        {
+            _client = client;
+            _queryStringHelper = new QueryStringHelper();
+        }
+
+        protected async Task<string> GetAsync(string url)
+        {
+            string content;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var response = await _client.SendAsync(requestMessage);
+            content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+
+            return content;
+        }
+
+        protected async Task<string> GetAsync(string url, object data)
+        {
+            string content;
+
+            var queryString = _queryStringHelper.GetQueryString(data);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{url}{queryString}");
+
+            var response = await _client.SendAsync(requestMessage);
+            content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            return content;
+        }
+
+        protected async Task<string> PostAsync(string url, string data)
+        {
+            string content;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(data, Encoding.UTF8, "application/json")
+            };
+
+            var response = await _client.SendAsync(requestMessage);
+            content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            return content;
+        }
+
+        protected async Task<string> PutAsync(string url, string data)
+        {
+            string content;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Put, url)
+            {
+                Content = new StringContent(data, Encoding.UTF8, "application/json")
+            };
+
+            var response = await _client.SendAsync(requestMessage);
+            content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            return content;
+        }
+
+        protected async Task<string> PatchAsync(string url, string data)
+        {
+            string content;
+
+            var requestMessage = new HttpRequestMessage(new HttpMethod("PATCH"), url)
+            {
+                Content = new StringContent(data, Encoding.UTF8, "application/json")
+            };
+
+            var response = await _client.SendAsync(requestMessage);
+            content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            return content;
+        }
+
+        protected async Task DeleteAsync(string url, string data)
+        {
+            HttpRequestMessage requestMessage;
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                requestMessage = new HttpRequestMessage(HttpMethod.Delete, url);
+            }
+            else
+            {
+                requestMessage = new HttpRequestMessage(HttpMethod.Delete, url)
+                {
+                    Content = new StringContent(data, Encoding.UTF8, "application/json")
+                };
+            }
+
+            var response = await _client.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ HttpClient/SFA.DAS.SecureHttpClient/MessageHandlers/DefaultHeadersHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Http.MessageHandlers
+{
+    public sealed class DefaultHeadersHandler : DelegatingHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage message, CancellationToken cancellationToken)
+        {
+            message.Headers.Add("api-version", "1");
+            message.Headers.Add("accept", "application/json");
+
+            return await base.SendAsync(message, cancellationToken);
+        }
+    }
+}
