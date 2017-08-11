18576bd0faf4c8d25b4532500182f4bb5ab31e91
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-11 10:32:12 +0100

    Moved interface to shared packages as it will be used service wide

---
+++ Configuration/SFA.DAS.Configuration/IConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Configuration
+{
+    public interface IConfiguration
+    {
+        string DatabaseConnectionString { get; set; }
+        string ServiceBusConnectionString { get; set; }
+    }
+}
