22f17c1dad3b7e015642648a532d2dc28231449b
froberg <christofer.froberg@gmail.com>
2017-08-23 12:00:36 +0100

    [Logging] added session id acrion filter

---
+++ Logging/SFA.DAS.NLog.Logger/Web/SessionIdActionFilter.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+using NLog;
+
+namespace SFA.DAS.NLog.Logger.Web
+{
+    public class SessionIdActionFilter : IActionFilter
+    {
+        private readonly HttpContext _httpContext;
+
+        public SessionIdActionFilter(HttpContext httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var sessionCookie = "SFA.DAS.Session";
+            var cookie = _httpContext.Request.Cookies[sessionCookie];
+            if (cookie == null)
+            {
+                var c = new HttpCookie(sessionCookie, Guid.NewGuid().ToString());
+                _httpContext.Response.Cookies.Add(c);
+                cookie = _httpContext.Response.Cookies[sessionCookie];
+            }
+
+            if (cookie != null)
+            {
+                MappedDiagnosticsLogicalContext.Set(Constants.HeaderNameSessionCorrelationId, cookie.Value);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+    }
+}
