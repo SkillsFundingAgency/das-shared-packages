22b7cfede4fe50c8c6feeb3166d94c2f2628504b
cousinst <trevor_cousins@yahoo.com>
2017-09-15 15:49:56 +0100

    Shared azure table config Needed in EAS, Activities, Tasks at least.

---
+++ Configuration/SFA.DAS.Configuration.AzureTableStorage/Environment/IConfigurationInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.Configuration.AzureTableStorage.Environment
+{
+    public interface IConfigurationInfo<T>
+    {
+        T GetConfiguration(string serviceName);
+        T GetConfiguration(string serviceName, Action<string> action);
+    }
+}
