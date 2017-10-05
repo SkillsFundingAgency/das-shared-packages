403cb32adf0f31052583d368bd2e21d52546ec4a
kola-samuel <samuel.dev.dba@gmail.com>
2017-10-05 10:59:46 +0100

    New  Shared Hashing Service code changes

---
+++ SFA.DAS.HashingService/SFA.DAS.HashingService.UnitTest/HashingServiceTest/WhenHashingNumbers.cs
@@ -0,0 +1,58 @@
+﻿using HashidsNet;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.HashingService.UnitTest.HashingServiceTest
+{
+    [TestFixture]
+    public class WhenHashingNumbers
+    {
+        private const string Hashstring = "SFA: digital apprenticeship service";
+        private const string AllowedCharacters = "46789BCDFGHJKLMNPRSTVWXY";
+
+        [Test]
+        public void When_AllowedCharacter_IsEmpty_Should_Throw_Exception()
+        {
+            //Act
+            ActualValueDelegate<object> testDelegate = () => new HashingService(string.Empty, Hashstring);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void When_Hashstring_IsEmpty_Should_Throw_Exception()
+        {
+            //Act
+            ActualValueDelegate<object> testDelegate = () => new HashingService(AllowedCharacters, string.Empty);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(123456)]
+        [TestCase(399999)]
+        [TestCase(233333)]
+        [TestCase(333333)]
+        [TestCase(444444)]
+        [TestCase(0)]
+        public void Then_HashValue_Should_Equal_DecodeValue(long valueToHash)
+        {
+            // Arrange 
+            var _sut = new HashingService(AllowedCharacters, Hashstring);
+
+            //Act
+            var hash = _sut.HashValue(valueToHash);
+            var actualValue = _sut.DecodeValue(hash);
+
+            //Assert
+            Assert.AreEqual(valueToHash, actualValue);
+        }
+
+    }
+}
---
+++ SFA.DAS.HashingService/SFA.DAS.HashingService/HashingService.cs
@@ -0,0 +1,30 @@
+﻿namespace SFA.DAS.HashingService
+{
+    using HashidsNet;
+    using System;
+
+    public class HashingService : IHashingService
+    {
+        private readonly Hashids _hashIds;
+      
+        public HashingService(string allowedCharacters, string hashstring)
+        {
+            if (string.IsNullOrEmpty(allowedCharacters) || string.IsNullOrEmpty(hashstring))
+            {
+                throw new ArgumentException("Hash String or Allowed Characters cannot be null or empty");
+            }
+            
+            _hashIds = new Hashids(hashstring, 6, allowedCharacters);
+        }
+
+        public string HashValue(long id)
+        {
+            return _hashIds.EncodeLong(id);
+        }
+
+        public long DecodeValue(string id)
+        {
+            return _hashIds.DecodeLong(id)[0];
+        }
+    }
+}
