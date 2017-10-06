c15a7b1012a0344567b19450520fcfe7504bf6dc
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-10-06 15:49:35 +0100

    Added azure queue implementation of the new pub/sub messaging code

---
+++ Messaging/SFA.DAS.Messaging.AzureStorageQueue/QueueMessagePublisher.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Messaging.AzureStorageQueue
+{
+    public class QueueMessagePublisher<T> : IMessagePublisher<T> where T : new()
+    {
+        private readonly string _connectionString;
+        private readonly string _queueName;
+
+        public QueueMessagePublisher(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+            _queueName = queueName;
+        }
+
+        public async Task PublishAsync(T message)
+        {
+            var content = JsonConvert.SerializeObject(message);
+
+            var queue = GetQueue();
+            await queue.AddMessageAsync(new CloudQueueMessage(content));
+        }
+
+        private CloudQueue GetQueue()
+        {
+
+            var storageAccount = CloudStorageAccount.Parse(_connectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            return queueClient.GetQueueReference(_queueName);
+        }
+    }
+}
---
+++ Messaging/SFA.DAS.Messaging.AzureStorageQueue/QueueMessageSubscriber.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace SFA.DAS.Messaging.AzureStorageQueue
+{
+    public class QueueMessageSubscriber<T> : IMessageSubscriber<T> where T : new()
+    {
+        private readonly string _connectionString;
+        private readonly string _queueName;
+
+        public QueueMessageSubscriber(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+            _queueName = queueName;
+        }
+
+        public async Task<IMessage<T>> ReceiveAsAsync()
+        {
+            var queue = GetQueue();
+            var message = await queue.GetMessageAsync();
+
+            if (message == null)
+            {
+                return null;
+            }
+
+            return new AzureStorageQueueMessage<T>(message, queue);
+        }
+
+        public async Task<IEnumerable<IMessage<T>>> ReceiveBatchAsAsync(int batchSize)
+        {
+            var queue = GetQueue();
+            var batch = await queue.GetMessagesAsync(batchSize);
+            return batch.Select(m => new AzureStorageQueueMessage<T>(m, queue));
+        }
+
+        public void Dispose()
+        {
+            //No clean up needed
+        }
+
+        private CloudQueue GetQueue()
+        {
+            var storageAccount = CloudStorageAccount.Parse(_connectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            return queueClient.GetQueueReference(_queueName);
+        }
+    }
+}
