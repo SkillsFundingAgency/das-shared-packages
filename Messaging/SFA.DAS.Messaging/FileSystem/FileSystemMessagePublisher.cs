9b565597f9bb51f7307cb0801dec5ed5abd9003f
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-10-06 15:51:34 +0100

    Added file system implementation of the new messaging code

---
+++ Messaging/SFA.DAS.Messaging/FileSystem/FileSystemMessagePublisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Messaging.FileSystem
+{
+    public class FileSystemMessagePublisher<T> : IMessagePublisher<T> where T : new()
+    {
+        private readonly string _storageDirectory;
+        private readonly string _topicName;
+
+        public FileSystemMessagePublisher(string storageDirectory, string topicName = "")
+        {
+            _storageDirectory = storageDirectory;
+            _topicName = topicName;
+        }
+
+        public async Task PublishAsync(T message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+
+            var directoryName = Path.Combine(_storageDirectory, _topicName);
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            var path = Path.Combine(directoryName, Guid.NewGuid() + ".json");
+            using (var stream = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            using (var writer = new StreamWriter(stream))
+            {
+                await writer.WriteAsync(json);
+                writer.Close();
+            }
+        }
+    }
+}
---
+++ Messaging/SFA.DAS.Messaging/FileSystem/FileSystemMessageSubscriber.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Messaging.FileSystem
+{
+    public class FileSystemMessageSubscriber<T> : IMessageSubscriber<T> where T : new()
+    {
+        private readonly string _storageDirectory;
+        private readonly string _topicName;
+       
+        public FileSystemMessageSubscriber(string storageDirectory, string topicName = "")
+        {
+            _storageDirectory = storageDirectory;
+            _topicName = topicName;
+        }
+
+        public async Task<IMessage<T>> ReceiveAsAsync()
+        {
+            var nextFile = GetAvailableMessages(_topicName).FirstOrDefault();
+
+            if (nextFile == null)
+            {
+                await Task.Delay(5000);
+                return null;
+            }
+
+            return await FileSystemMessage<T>.Lock(nextFile);
+        }
+
+        public async Task<IEnumerable<IMessage<T>>> ReceiveBatchAsAsync(int batchSize)
+        {
+            var availableMessageFiles = GetAvailableMessages(_topicName).Take(batchSize).ToArray();
+            if (!availableMessageFiles.Any())
+            {
+                return new Message<T>[0];
+            }
+
+            var messages = new Message<T>[availableMessageFiles.Length];
+            for (var i = 0; i < messages.Length; i++)
+            {
+                messages[i] = await FileSystemMessage<T>.Lock(availableMessageFiles[i]);
+            }
+            return messages;
+        }
+
+        public void Dispose()
+        {
+           //No clean up needed for file subscriber
+        }
+
+        private IEnumerable<FileInfo> GetAvailableMessages(string messagePath)
+        {
+            var directory = new DirectoryInfo(Path.Combine(_storageDirectory, messagePath));
+            if (!directory.Exists)
+            {
+                return new FileInfo[0];
+            }
+
+            var jsonFiles = directory.GetFiles("*.json");
+            var lockFiles = directory.GetFiles("*.lck");
+            return jsonFiles
+                .Where(jf => !lockFiles.Any(lf => lf.Name.StartsWith(jf.Name)))
+                .OrderByDescending(jf => jf.LastWriteTimeUtc);
+        }
+    }
+}
