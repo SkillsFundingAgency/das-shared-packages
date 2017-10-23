7372037ff546a5898b5967de6735838b9816981b
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-10-23 09:35:45 +0100

    Added initial common enum type

---
+++ SFA.DAS.Common/SFA.DAS.Common.Domain/Types/OrganisationType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace SFA.DAS.Common.Domain.Types
+{
+    public enum OrganisationType : short
+    {
+        [Description("Listed on Companies House")]
+        CompaniesHouse = 1,
+        [Description("Charities")]
+        Charities = 2,
+        [Description("Public Bodies")]
+        PublicBodies = 3,
+        [Description("Other")]
+        Other = 4
+    }
+}
