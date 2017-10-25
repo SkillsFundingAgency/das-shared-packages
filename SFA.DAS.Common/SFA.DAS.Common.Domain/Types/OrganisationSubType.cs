9e370c79705d1244b5746a11cc20aaad74762633
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-10-25 15:33:16 +0100

    Added OrganisationSubType enum

---
+++ SFA.DAS.Common/SFA.DAS.Common.Domain/Types/OrganisationSubType.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Common.Domain.Types
+{
+    public enum OrganisationSubType
+    {
+        None = 0,
+
+        // Public sector
+        Ons = 1,
+        Nhs = 2,
+        Police = 3
+    }
+}
