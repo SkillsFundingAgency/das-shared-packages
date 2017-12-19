0228efd2830e7e899aa8a5db3e63eb37a19ab180
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-12-19 09:44:12 +0000

    Created new shared package

---
+++ SFA.DAS.Support.Shared/SFA.DAS.Support.Shared/ResultPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Support.Shared
+{
+    [ExcludeFromCodeCoverage]
+    public class ResultPage<T>
+    {
+        [JsonProperty("_links")]
+        public PageLinks Links { get; set; }
+
+        public IEnumerable<T> Results { get; set; }
+        public int Size { get; set; }
+        public int Start { get; set; }
+    }
+}
---
+++ SFA.DAS.Support.Shared/SFA.DAS.Support.Shared/SiteResource.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.Support.Shared
+{
+    [ExcludeFromCodeCoverage]
+    public class SiteResource
+    {
+        public string ResourceKey { get; set; }
+        public string ResourceUrlFormat { get; set; }
+        public string SearchItemsUrl { get; set; }
+        public string ResourceTitle { get; set; }
+        public string Challenge { get; set; }
+    }
+}
