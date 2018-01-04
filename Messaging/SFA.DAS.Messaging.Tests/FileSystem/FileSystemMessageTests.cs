52e1fc70e078d26a428f76b46dbe6cd747209b6b
Robin O'Connor <github@bytepointsoftware.com>
2018-01-04 11:16:07 +0000

    Added support for private property setters during Json serialization for messages

---
+++ Messaging/SFA.DAS.Messaging.Tests/FileSystem/FileSystemMessageTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.Messaging.FileSystem;
+
+namespace SFA.DAS.Messaging.UnitTests.FileSystem
+{
+    public class FileSystemMessageTests
+    {
+        private const string TestFilePath = "TestFile.json";
+        private const string ExpectedData = "Some Data";
+
+        [SetUp]
+        public void Arrange()
+        {
+            var serialisedContent = JsonConvert.SerializeObject(new PrivateSetterTestClass(ExpectedData));
+
+            File.WriteAllText(TestFilePath, serialisedContent);
+        }
+
+        [Test]
+        public async Task ShouldDeserialisePrivateSetters()
+        {
+            //Act
+            var result = await FileSystemMessage<PrivateSetterTestClass>.Lock(new FileInfo(TestFilePath));
+
+            //Assert
+            Assert.AreEqual(ExpectedData, result.Content.Data);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            File.Delete(TestFilePath);
+            File.Delete($"{TestFilePath}.lck");
+        }
+        
+        internal class PrivateSetterTestClass
+        {
+            public string Data { get; protected set; }
+
+            public PrivateSetterTestClass()
+            { }
+
+            public PrivateSetterTestClass(string data)
+            {
+                Data = data;
+            }
+        }
+    }
+}
--- Messaging/SFA.DAS.Messaging/FileSystem/FileSystemMessage.cs
+++ Messaging/SFA.DAS.Messaging/FileSystem/FileSystemMessage.cs
@@ -1,6 +1,8 @@
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace SFA.DAS.Messaging.FileSystem
 {
@@ -38,12 +40,37 @@ namespace SFA.DAS.Messaging.FileSystem
             }
 
             using (var stream = new FileStream(dataFile.FullName, FileMode.Open, FileAccess.Read))
+
             using (var reader = new StreamReader(stream))
             {
+                var jsonSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new PrivateSetterJsonContractResolver()
+                };
+
                 var json = await reader.ReadToEndAsync();
-                var content = JsonConvert.DeserializeObject<T>(json);
+                var content = JsonConvert.DeserializeObject<T>(json, jsonSettings);
 
                 return new FileSystemMessage<T>(dataFile, lockFile, content);
+            }
+        }
+
+        internal class PrivateSetterJsonContractResolver : DefaultContractResolver
+        {
+            protected override JsonProperty CreateProperty(
+                MemberInfo member,
+                MemberSerialization memberSerialization)
+            {
+                var prop = base.CreateProperty(member, memberSerialization);
+                var property = member as PropertyInfo;
+
+                if (prop.Writable || property == null)
+                    return prop;
+
+                var hasPrivateSetter = property.GetSetMethod(true) != null;
+                prop.Writable = hasPrivateSetter;
+
+                return prop;
             }
         }
     }
