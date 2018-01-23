71d4184ea0b844e1243b3bee9567f139a2ca4629
Iain Stevenson <31848657+IainStevenson@users.noreply.github.com>
2018-01-23 16:20:55 +0000

    Move of SFA.DAS.Support.Sahred into SFA.DAS.Support.Portal repository (#193)

--- SFA.DAS.Support.Shared/SFA.DAS.Support.Shared/IClientValidatior.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SFA.DAS.Support.Shared
-{
-    public interface IClientValidatior
-    {
-        Task<bool> Validate(string token, string clientId, string appKey, string resourceId, string tenant);
-    }
-}
