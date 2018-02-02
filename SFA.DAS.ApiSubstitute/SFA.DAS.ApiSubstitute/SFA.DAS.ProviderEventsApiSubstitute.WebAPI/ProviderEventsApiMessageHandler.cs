370fbf68cacef1422222d5df9c5a89fb39f0e6c0
Arvinth Seran <arvinth.seran@gmail.com>
2018-02-02 15:17:53 +0000

    default values are made visible

--- SFA.DAS.ApiSubstitute/SFA.DAS.ApiSubstitute/SFA.DAS.ProviderEventsApiSubstitute.WebAPI/ProviderEventsApiMessageHandler.cs
+++ SFA.DAS.ApiSubstitute/SFA.DAS.ApiSubstitute/SFA.DAS.ProviderEventsApiSubstitute.WebAPI/ProviderEventsApiMessageHandler.cs
@@ -10,6 +10,9 @@ namespace SFA.DAS.ProviderEventsApiSubstitute.WebAPI
 
         public string GetSubmissionEventsEndPoint => "api/submissions?page=1";
 
+        public const long ApprenticeshipId = 45785214;
+        public const long Ukprn = 10000254;
+
         public ProviderEventsApiMessageHandler(string baseAddress) : base(baseAddress)
         {
             _objectCreator = new ObjectCreator();
@@ -29,7 +32,7 @@ namespace SFA.DAS.ProviderEventsApiSubstitute.WebAPI
 
         private void ConfigureGetSubmissionEvents()
         {
-            var submissionEvents = _objectCreator.Create<SubmissionEvent>(x => { x.ApprenticeshipId = 45785214; x.Ukprn = 10000254; });
+            var submissionEvents = _objectCreator.Create<SubmissionEvent>(x => { x.ApprenticeshipId = ApprenticeshipId ; x.Ukprn = Ukprn; });
 
             SetupGet(GetSubmissionEventsEndPoint, new PageOfResults<SubmissionEvent> { Items = new[] { submissionEvents }, PageNumber = 1, TotalNumberOfPages = 1 });
         }
