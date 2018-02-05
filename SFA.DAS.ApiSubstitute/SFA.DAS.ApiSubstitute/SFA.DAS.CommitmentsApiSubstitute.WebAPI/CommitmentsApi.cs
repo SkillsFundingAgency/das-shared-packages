d640bd550ac1a6d1c695aa531b5ed3f48750fe29
Arvinth Seran <arvinth.seran@gmail.com>
2018-02-05 16:24:17 +0000

    commitments api stub

---
+++ SFA.DAS.ApiSubstitute/SFA.DAS.ApiSubstitute/SFA.DAS.CommitmentsApiSubstitute.WebAPI/CommitmentsApi.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.ApiSubstitute.WebAPI;
+
+namespace SFA.DAS.CommitmentsApiSubstitute.WebAPI
+{
+    public class CommitmentsApi : WebApiSubstitute
+    {
+        public string BaseAddress { get; private set; }
+
+        public CommitmentsApiMessageHandler CommitmentsApiMessageHandler { get; private set; }
+
+        public CommitmentsApi(CommitmentsApiMessageHandler apiMessageHandler) : base(apiMessageHandler)
+        {
+            BaseAddress = apiMessageHandler.BaseAddress;
+            CommitmentsApiMessageHandler = apiMessageHandler;
+        }
+    }
+}
---
+++ SFA.DAS.ApiSubstitute/SFA.DAS.ApiSubstitute/SFA.DAS.CommitmentsApiSubstitute.WebAPI/CommitmentsApiMessageHandler.cs
@@ -0,0 +1,41 @@
+﻿using SFA.DAS.ApiSubstitute.Utilities;
+using SFA.DAS.ApiSubstitute.WebAPI.MessageHandlers;
+using SFA.DAS.Commitments.Api.Types.Apprenticeship;
+using System;
+
+
+namespace SFA.DAS.CommitmentsApiSubstitute.WebAPI
+{
+    public class CommitmentsApiMessageHandler : ApiMessageHandlers
+    {
+        private IObjectCreator _objectCreator;
+
+        public string GetProviderApprenticeship => $"api/provider/{ProviderId}/apprenticeships/{ApprenticeshipId}";
+
+        public const long ProviderId = 10002457;
+        public const long ApprenticeshipId = 45784125;
+
+        public CommitmentsApiMessageHandler(string baseAddress) : base(baseAddress)
+        {
+            _objectCreator = new ObjectCreator();
+            ConfigureDefaultResponse();
+        }
+
+        public void ConfigureDefaultResponse()
+        {
+            ConfigureGetProviderApprenticeship();
+        }
+
+        public void OverrideGetProviderApprenticeship<T>(T response)
+        {
+            SetupPut(GetProviderApprenticeship);
+            SetupGet(GetProviderApprenticeship, response);
+        }
+
+        public void ConfigureGetProviderApprenticeship()
+        {
+            var apprenticeship = _objectCreator.Create<Apprenticeship>(x => { x.ProviderId = ProviderId; x.Id = ApprenticeshipId; });
+            SetupGet(GetProviderApprenticeship, apprenticeship);
+        }
+    }
+}
