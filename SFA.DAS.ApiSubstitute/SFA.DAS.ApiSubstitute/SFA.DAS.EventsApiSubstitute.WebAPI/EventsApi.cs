69c36df6a60ff13a399eced835ac8a1b452bea7b
Arvinth Seran <arvinth.seran@gmail.com>
2018-02-22 11:07:00 +0000

    added events api subs

---
+++ SFA.DAS.ApiSubstitute/SFA.DAS.ApiSubstitute/SFA.DAS.EventsApiSubstitute.WebAPI/EventsApi.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.ApiSubstitute.WebAPI;
+
+namespace SFA.DAS.EventsApiSubstitute.WebAPI
+{
+    public class EventsApi : WebApiSubstitute
+    {
+        public string BaseAddress { get; private set; }
+
+        public EventsApiMessageHandler AccountsApiMessageHandler { get; private set; }
+
+        public EventsApi(EventsApiMessageHandler apiMessageHandler) : base(apiMessageHandler)
+        {
+            BaseAddress = apiMessageHandler.BaseAddress;
+            AccountsApiMessageHandler = apiMessageHandler;
+        }
+    }
+}
