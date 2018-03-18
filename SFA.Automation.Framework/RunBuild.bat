163e61d48846d5bc82c41fe5c5fd0e885ba2a500
Arvinth Seran <arvinth.seran@gmail.com>
2018-03-18 15:53:51 +0000

    downgraded fakebuild nuget package

--- SFA.Automation.Framework/RunBuild.bat
+++ SFA.Automation.Framework/RunBuild.bat
@@ -1,6 +1,4 @@
 "tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
 "tools\nuget\nuget.exe" "install" "NUnit.Console" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "xunit.runner.console" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "PhantomJS" "-OutputDirectory" "tools" "-ExcludeVersion"
 
-"tools\FAKE\tools\FAKE.exe" "Build.fsx" %2 "publishDirectory=Publish" "buildMode=%1" "playList=%3"
+tools\FAKE\tools\FAKE.exe Build.fsx publishDirectory="Publish" buildMode=%1 target=%2
