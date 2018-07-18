8bbb6e8d8ae51bdad1b3071dfc55e1cfb4d58ea7
Matt Derry <matt@goodoldmatt.com>
2018-07-18 14:25:07 +0100

    Moves NServiceBus UnitOfWork endpoint extension method

--- SFA.DAS.NServiceBus/Sfa.Das.NServiceBus/EndpointConfigurationExtensions.cs
+++ SFA.DAS.NServiceBus/Sfa.Das.NServiceBus/EndpointConfigurationExtensions.cs
@@ -58,6 +58,23 @@ namespace SFA.DAS.NServiceBus
             return config;
         }
 
-      
+        public static EndpointConfiguration SetupUnitOfWork(this EndpointConfiguration config)
+        {
+            config.RegisterComponents(c =>
+            {
+                if (!c.HasComponent<IDb>())
+                {
+                    c.ConfigureComponent<Db>(DependencyLifecycle.InstancePerUnitOfWork);
+                }
+
+                c.ConfigureComponent<EventPublisher>(DependencyLifecycle.InstancePerUnitOfWork);
+                c.ConfigureComponent<UnitOfWorkContext>(DependencyLifecycle.InstancePerUnitOfWork);
+                c.ConfigureComponent<UnitOfWorkManager>(DependencyLifecycle.InstancePerUnitOfWork);
+            });
+
+            config.Pipeline.Register(new UnitOfWorkBehavior(), "Sets up a unit of work for each message");
+
+            return config;
+        }
     }
 }
