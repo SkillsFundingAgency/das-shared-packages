68a07ceb033d417d2be57c54bff6cccb261b1914
Matt Derry <matt@goodoldmatt.com>
2018-08-14 15:11:04 +0100

    Adds nservicebus .net core endpoint service extension

---
+++ SFA.DAS.NServiceBus/Sfa.Das.NServiceBus/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+namespace SFA.DAS.NServiceBus
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddNServiceBus(this IServiceCollection services, EndpointConfiguration endpointConfiguration)
+        {
+            var endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            services.AddSingleton(endpoint);
+            services.AddSingleton<IMessageSession>(endpoint);
+            return services;
+        }
+    }
+}
