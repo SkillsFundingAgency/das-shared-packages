b0cf79982b4166539bb2b492db43ee591d65b807
Ian Gledhill <ian@gledhills.mobi>
2018-10-23 08:42:39 +0100

    Add object to dictionary extension

---
+++ Messaging/SFA.DAS.Messaging.Tests/ObjectExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.Messaging.UnitTests
+{
+    [TestFixture]
+    public class ObjectExtensionTests
+    {
+        [Test]
+        public void ToDictionary()
+        {
+            var testClass = new TestProperties1();
+            var dictionary = testClass.ToDictionary();
+
+            Assert.IsTrue(dictionary.ContainsKey(nameof(TestProperties1.IntProperty)));
+            Assert.IsTrue(dictionary.ContainsKey(nameof(TestProperties1.StringProperty)));
+            Assert.IsTrue(dictionary.ContainsKey(nameof(TestProperties1.ReadOnlyProperty)));
+        }
+    }
+
+    internal class TestProperties1
+    {
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+        public DateTime ReadOnlyProperty => DateTime.Today;
+    }
+}
