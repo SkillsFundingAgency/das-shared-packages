cbc0e0f0f1c4ccacc099f2d1310222e1b089ca74
tom-gough <tom@tgsd.co.uk>
2018-11-28 11:09:46 +0000

    Added a registry to register types for auto configuration

---
+++ SFA.DAS.AutoConfiguration/SFA.DAS.AutoConfiguration/DependencyResolution/AutoConfigurationRegistry.cs
@@ -0,0 +1,14 @@
+﻿using StructureMap;
+
+namespace SFA.DAS.AutoConfiguration.DependencyResolution
+{
+    public class AutoConfigurationRegistry : Registry
+    {
+        public AutoConfigurationRegistry()
+        {
+            For<ITableStorageConfigurationService>().Use<TableStorageConfigurationService>();
+            For<IEnvironmentService>().Use<EnvironmentService>();
+            For<IAzureTableStorageConnectionAdapter>().Use<AzureTableStorageConnectionAdapter>();
+        }
+    }
+}
