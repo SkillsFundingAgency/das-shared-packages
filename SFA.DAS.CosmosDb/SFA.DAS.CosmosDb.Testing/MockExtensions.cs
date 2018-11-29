5b9306f524cc443d8dc83d6bdeb7ab6ce49c9936
Paul Graham <paul.graham@amido.com>
2018-11-29 10:33:16 +0000

    Added a MockExtensiion method  SetupImMemoryCollection() to the Testing project. This is using Moq version 4.8.0 as this was the lowest version which works with .NetStandard2

---
+++ SFA.DAS.CosmosDb/SFA.DAS.CosmosDb.Testing/MockExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Moq;
+
+namespace SFA.DAS.CosmosDb.Testing
+{
+    public static class MockExtensions
+    {
+        public static void SetupInMemoryCollection<TRepository, TDocument>(this Mock<TRepository> documentRepository, IEnumerable<TDocument> documents) where TRepository : class, IDocumentRepository<TDocument> where TDocument : class
+        {
+            var documentQuery = new FakeDocumentQuery<TDocument>(documents);
+
+            documentRepository.Setup(r => r.CreateQuery(null)).Returns(documentQuery);
+        }
+    }
+}
