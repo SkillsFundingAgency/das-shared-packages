74fdff9b6c5a3d406776048e2535b1e247507f01
sachin.patel <sachin.patel@valtech.co.uk>
2018-12-04 09:20:16 +0000

    :pencil: - Adding a guid id to VacancyEvent

--- SFA.DAS.Vacancy.Analytics.Client/SFA.DAS.Vacancy.Analytics.Client/Events/VacancyEvent.cs
+++ SFA.DAS.Vacancy.Analytics.Client/SFA.DAS.Vacancy.Analytics.Client/Events/VacancyEvent.cs
@@ -4,9 +4,15 @@ namespace Esfa.Vacancy.Analytics.Events
 {
     public abstract class VacancyEvent
     {
+        public Guid Id { get; set; }
         public long VacancyReference { get; protected set; }
         public abstract string EventType { get; }
         public abstract DateTime EventTime { get; protected set; }
         public abstract string PublisherId { get; protected set; }
+
+        public VacancyEvent()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
