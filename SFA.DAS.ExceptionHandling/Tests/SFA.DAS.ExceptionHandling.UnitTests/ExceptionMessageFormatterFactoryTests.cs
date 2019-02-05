42791cc5ebb88f5dd9a505bc3f3786aeac656171
Ian Gledhill <ian@gledhills.mobi>
2019-02-05 09:23:28 +0000

    Change test lib to framework

--- SFA.DAS.ExceptionHandling/Tests/SFA.DAS.ExceptionHandling.UnitTests/ExceptionMessageFormatterFactoryTests.cs
+++ SFA.DAS.ExceptionHandling/Tests/SFA.DAS.ExceptionHandling.UnitTests/ExceptionMessageFormatterFactoryTests.cs
@@ -11,24 +11,29 @@ namespace SFA.DAS.ExceptionHandling.UnitTests
         [Test]
         public void Constructor_ValidCall_ShouldNotThrowException()
         {
+            // Arrange
             var fixtures = new ExceptionMessageFormatterFactoryTestFixtures();
 
+            // Act
             var factory = fixtures.CreateFactory();
 
+            // Assert
             Assert.Pass("The factory was created successfully");
         }
 
         [TestCase(typeof(Exception), typeof(ExceptionMessageFormatter))]
         public void GetFormatter_ForException_ShouldGetExceptionFormatter(Type exceptionType, Type expectedFormatterType)
         {
+            // Arrange
             var fixtures = new ExceptionMessageFormatterFactoryTestFixtures();
             var factory = fixtures.CreateFactory();
-
             var exception = Activator.CreateInstance(exceptionType) as Exception;
 
+            // Actual
             var actualFormatterType = factory.GetFormatter(exception);
 
-            Assert.AreEqual(expectedFormatterType, actualFormatterType);
+            // Assert
+            Assert.AreEqual(expectedFormatterType, actualFormatterType.GetType());
         }
     }
 
