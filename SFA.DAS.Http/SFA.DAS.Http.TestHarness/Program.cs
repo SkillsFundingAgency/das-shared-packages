590dd99676f2296050679c547251d3719e64f7de
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2019-02-05 14:17:21 +0000

    Added test harness to allow for the Rest client to be tested

---
+++ SFA.DAS.Http/SFA.DAS.Http.TestHarness/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using SFA.DAS.Http.REST;
+
+namespace SFA.DAS.Http.TestHarness
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+ 
+            host.Start();
+            
+            Console.WriteLine("Creating Http client");
+            var httpClient = new HttpClientBuilder().WithDefaultHeaders().Build();
+            httpClient.BaseAddress = new Uri("http://localhost:5000/api/");
+
+            Console.WriteLine("Creating REST client");
+            var restClient = new RestHttpClient(httpClient);
+
+            Console.WriteLine("Calling test server api to get test value...");
+            var result = restClient.Get("test").Result;
+            
+            Console.WriteLine($"Api Test Call Result: {result}");
+
+            Console.WriteLine("Press any key to quit...");
+            Console.Read();
+
+            host.StopAsync().Wait();
+        }
+    }
+}
