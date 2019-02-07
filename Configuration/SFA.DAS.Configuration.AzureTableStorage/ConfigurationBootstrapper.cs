4046bd59a141c6791757d3023cd9a066265b5658
Phil Davies <github@phildavies.co.uk>
2019-02-07 08:29:59 +0000

    configuration bootstrapper: default to storage emulator if no storage connection string environment variable when developing locally

--- Configuration/SFA.DAS.Configuration.AzureTableStorage/ConfigurationBootstrapper.cs
+++ Configuration/SFA.DAS.Configuration.AzureTableStorage/ConfigurationBootstrapper.cs
@@ -3,19 +3,29 @@ using Microsoft.Extensions.Configuration;
 
 namespace SFA.DAS.Configuration.AzureTableStorage
 {
-    public class ConfigurationBootstrapper
+    public static class ConfigurationBootstrapper
     {
-        private const string DefaultEnvironment = "LOCAL";
+        private const string DeveloperEnvironment = "LOCAL";
+        private const string DeveloperEnvironmentDefaultStorageConnectionString = "UseDevelopmentStorage=true";
 
         public static (string StorageConnectionString, string EnvironmentName) GetEnvironmentVariables()
         {
             var environmentVariablesConfig = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+            var environmentName = environmentVariablesConfig[EnvironmentVariableNames.Environment] ?? DeveloperEnvironment;
             
             var storageConnectionString = environmentVariablesConfig[EnvironmentVariableNames.ConfigurationStorageConnectionString];
             if (string.IsNullOrWhiteSpace(storageConnectionString))
-                throw new Exception($"Hey {environmentVariablesConfig["USERNAME"]??"developer"}, you need to set the environment variable '{EnvironmentVariableNames.ConfigurationStorageConnectionString}'. Set it to a connection string pointing to a storage account containing a 'Configuration' table. See readme.md for more information.");
-
-            var environmentName = environmentVariablesConfig[EnvironmentVariableNames.Environment] ?? DefaultEnvironment;
+            {
+                if (string.Equals(environmentName, DeveloperEnvironment, StringComparison.OrdinalIgnoreCase))
+                {
+                    storageConnectionString = DeveloperEnvironmentDefaultStorageConnectionString;
+                }
+                else
+                {
+                    throw new Exception($"Missing environment variable '{EnvironmentVariableNames.ConfigurationStorageConnectionString}'. It should be present and set to a connection string pointing to the storage account containing a 'Configuration' table.");
+                }
+            }
 
             return (storageConnectionString, environmentName);
         }
---
+++ Configuration/SFA.DAS.Configuration.UnitTests/AzureTableStorage/ConfigurationBootstrapperTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.Configuration.UnitTests.AzureTableStorage
+{
+    [TestFixture]
+    [Parallelizable]
+    public class ConfigurationBootstrapperTests : FluentTest<ConfigurationBootstrapperTestsFixture>
+    {
+        [Test]
+        public void When()
+        {
+            //Test(f => f.);
+        }
+    }
+
+    public class ConfigurationBootstrapperTestsFixture
+    {
+        public ConfigurationBootstrapperTestsFixture()
+        {
+            
+        }
+    }
+}
