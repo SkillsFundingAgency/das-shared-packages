cf5d71a1fcb93544edd450f4cbf14b27ac484edc
Ian Gledhill <ian@gledhills.mobi>
2019-02-15 11:06:42 +0000

    CV-70-AddPost

--- SFA.DAS.Http/SFA.DAS.Http.UnitTests/RestHttpClientTests.cs
+++ SFA.DAS.Http/SFA.DAS.Http.UnitTests/RestHttpClientTests.cs
@@ -19,7 +19,7 @@ namespace SFA.DAS.Http.UnitTests.REST
         [Test]
         public Task WhenCallingGetAndHttpClientReturnsSuccess_ThenShouldReturnResponseBodyAsString()
         {
-            return RunAsync(f => f.SetupHttpClientGetToReturnSuccessWithStringResponseBody(), f => f.CallGet(null), 
+            return RunAsync(f => f.SetupHttpClientToReturnSuccessWithStringResponseBody(), f => f.CallGet(null), 
                 (f, r) =>
                 {
                     r.Should().NotBeNull();
@@ -30,7 +30,7 @@ namespace SFA.DAS.Http.UnitTests.REST
         [Test]
         public Task WhenCallingGenericGetAndHttpClientReturnsSuccess_ThenShouldReturnResponseBodyAsObject()
         {
-            return RunAsync(f => f.SetupHttpClientGetToReturnSuccessWithJsonObjectInResponseBody(), f => f.CallGetObject(null), 
+            return RunAsync(f => f.SetupHttpClientToReturnSuccessWithJsonObjectInResponseBody(), f => f.CallGetObject(null), 
                 (f, r) =>
                 {
                     r.Should().NotBeNull();
@@ -52,10 +52,63 @@ namespace SFA.DAS.Http.UnitTests.REST
         [Test]
         public Task WhenCallingGetAndQueryDataIsSupplied_ThenUriCalledShouldContainTheQueryData()
         {
-            return RunAsync(f => f.SetupHttpClientGetToReturnSuccessWithStringResponseBody(),
+            return RunAsync(f => f.SetupHttpClientToReturnSuccessWithStringResponseBody(),
                 f => f.CallGet(new {QueryParam1 = "qp1", QueryParam2 = "qp2"}),
                 (f, r) => f.HttpMessageHandler.HttpRequestMessage.RequestUri.Should().Be($"{f.HttpClient.BaseAddress}?QueryParam1=qp1&QueryParam2=qp2"));
         }
+
+        [Test]
+        public Task WhenCallingPostAsJsonExpectingStringAndHttpClientReturnsSuccess_ThenShouldReturnResponseBodyAsString()
+        {
+            const string expectedResponse = "Hello World!";
+
+            return RunAsync(
+                f => f.SetupHttpClientToReturnSuccessWithStringResponseBody(expectedResponse), 
+                f => f.CallPostAsJson(new {Property1 = "Hello who?"}),
+                (f, r) => r.Should().Be(expectedResponse));
+        }
+
+        [Test]
+        public Task WhenCallingPostAsJsonExpectingObjectAndHttpClientReturnsSuccess_ThenShouldReturnResponseBodyAsObject()
+        {
+            var expectedResponse = new TestObjectToUseAsAResponse
+            {
+                BoolField = true,
+                DateTimeField = DateTime.Now,
+                IntField = new Random().Next(),
+                StringField = Guid.NewGuid().ToString()
+            };
+
+            return RunAsync(
+                f => f.SetupHttpClientToReturnSuccessWithJsonObjectInResponseBody(expectedResponse),
+                f => f.CallPostAsJson<TestObjectToUseAsARequest, TestObjectToUseAsAResponse>(new TestObjectToUseAsARequest {StringField = "Hello who?"}),
+                (f, r) => r.Should().BeEquivalentTo(expectedResponse));
+        }
+
+
+        [Test]
+        public Task WhenCallingPostAsJsonAndHttpClientReturnsNonSuccess_ThenShouldThrowRestClientException()
+        {
+            return RunAsync(f => f.SetupHttpClientPostToReturnInternalServerErrorWithStringResponseBody(), f => f.CallGet(null),
+                (f, r) => r.Should().Throw<RestHttpClientException>()
+                    .Where(ex => ex.StatusCode == HttpStatusCode.InternalServerError
+                                 && ex.ReasonPhrase == "Internal Server Error"
+                                 && Equals(ex.RequestUri, f.RequestUri)
+                                 && ex.ErrorResponse.Contains(f.ResponseString)));
+        }
+    }
+
+    public class TestObjectToUseAsARequest
+    {
+        public string StringField { get; set; }
+    }
+
+    public class TestObjectToUseAsAResponse
+    {
+        public string StringField { get; set; }
+        public int IntField { get; set; }
+        public DateTime DateTimeField { get; set; }
+        public bool BoolField { get; set; }
     }
 
     public class RestClientTestsFixture
@@ -79,28 +132,42 @@ namespace SFA.DAS.Http.UnitTests.REST
             RestHttpClient = new RestHttpClient(HttpClient);
         }
         
-        public void SetupHttpClientGetToReturnSuccessWithStringResponseBody()
+        public void SetupHttpClientToReturnSuccessWithStringResponseBody(string responseString = "Test")
         {
-            ResponseString = "Test";
+            ResponseString = responseString;
             HttpMessageHandler.HttpResponseMessage = new HttpResponseMessage { Content = new StringContent(ResponseString, Encoding.Default, "text/plain") };
         }
 
-        public void SetupHttpClientGetToReturnSuccessWithJsonObjectInResponseBody()
+        public void SetupHttpClientToReturnSuccessWithJsonObjectInResponseBody()
         {
-            ResponseObject = new ExampleResponseObject {StringProperty = "Test property"};
-                
-            var stringBody = JsonConvert.SerializeObject(ResponseObject);
+            SetupHttpClientToReturnSuccessWithJsonObjectInResponseBody(new ExampleResponseObject { StringProperty = "Test property" });
+        }
+
+        public void SetupHttpClientToReturnSuccessWithJsonObjectInResponseBody<TResponse>(TResponse responseData)
+        {
+            var stringBody = JsonConvert.SerializeObject(responseData);
+            ResponseObject = responseData;
             HttpMessageHandler.HttpResponseMessage = new HttpResponseMessage { Content = new StringContent(stringBody, Encoding.Default, "application/json") };
         }
 
         public void SetupHttpClientGetToReturnInternalServerErrorWithStringResponseBody()
+        {
+            SetupHttpClientToReturnInternalServerErrorWithStringResponseBody(HttpMethod.Get);
+        }
+
+        public void SetupHttpClientPostToReturnInternalServerErrorWithStringResponseBody()
+        {
+            SetupHttpClientToReturnInternalServerErrorWithStringResponseBody(HttpMethod.Post);
+        }
+
+        public void SetupHttpClientToReturnInternalServerErrorWithStringResponseBody(HttpMethod httpMethod)
         {
             ResponseString = "Some sort of error description";
             RequestUri = new Uri($"{HttpClient.BaseAddress}/request", UriKind.Absolute);
             HttpMessageHandler.HttpResponseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError)
             {
                 Content = new StringContent(ResponseString, Encoding.Default, "text/plain"),
-                RequestMessage = new HttpRequestMessage(HttpMethod.Get, RequestUri)
+                RequestMessage = new HttpRequestMessage(httpMethod, RequestUri)
             };
         }
 
@@ -112,6 +179,16 @@ namespace SFA.DAS.Http.UnitTests.REST
         public async Task<ExampleResponseObject> CallGetObject(object queryData)
         {
             return await RestHttpClient.Get<ExampleResponseObject>("https://example.com", queryData);
+        }
+
+        public Task<string> CallPostAsJson<TRequestData>(TRequestData requestData)
+        {
+            return RestHttpClient.PostAsJson<TRequestData>("https://example.com", requestData);
+        }
+
+        public Task<TResponseData> CallPostAsJson<TRequestData, TResponseData>(TRequestData requestData)
+        {
+            return RestHttpClient.PostAsJson<TRequestData, TResponseData>("https://example.com", requestData);
         }
     }
 }
--- SFA.DAS.Http/SFA.DAS.Http/IRestHttpClient.cs
+++ SFA.DAS.Http/SFA.DAS.Http/IRestHttpClient.cs
@@ -10,5 +10,21 @@ namespace SFA.DAS.Http
         Task<string> Get(string uri, object queryData = null, CancellationToken cancellationToken = default);
         Task<T> Get<T>(Uri uri, object queryData = null, CancellationToken cancellationToken = default);
         Task<T> Get<T>(string uri, object queryData = null, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        ///     Serialize the supplied requestData into json and post to the specified URI.
+        /// </summary>
+        /// <returns>
+        ///     The content of the response as a string.
+        /// </returns>
+        Task<string> PostAsJson<TRequest>(string uri, TRequest requestData, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        ///     Serialize the supplied requestData into json and post to the specified URI.
+        /// </summary>
+        /// <returns>
+        ///     The content of the response de-serialized into an instance of TResponse.
+        /// </returns>
+        Task<TResponse> PostAsJson<TRequest, TResponse>(string uri, TRequest requestData, CancellationToken cancellationToken = default);
     }
 }
