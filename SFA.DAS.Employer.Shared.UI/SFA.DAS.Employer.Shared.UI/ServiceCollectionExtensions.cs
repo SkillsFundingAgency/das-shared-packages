c17177f2e487cbfae08a41d04b0edf2bed1ea1e2
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-03-04 14:07:50 +0000

    Add table row key value when pulling back config section

--- SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/ServiceCollectionExtensions.cs
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/ServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.Employer.Shared.UI
             ValidateArguments(logoutRouteName, identityClientId);
             
             // TODO: Validate configuration values?
-            services.Configure<MaMenuConfiguration>(configuration.GetSection("MaPageConfiguration"));
+            services.Configure<MaMenuConfiguration>(configuration.GetSection("SFA.DAS.Employer.Menu:MaPageConfiguration"));
             services.PostConfigure<MaMenuConfiguration>(options =>
             {
                 options.ClientId = identityClientId;
