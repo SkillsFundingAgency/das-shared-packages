ffb9d205c59a38d0c9c9fab6a0d1b5a9278b5fc8
Elliot Ritchie <elliotritchie@hotmail.com>
2019-05-13 17:00:29 +0100

    Renamed _encodings to _hashids

--- SFA.DAS.Encoding/SFA.DAS.Encoding/EncodingService.cs
+++ SFA.DAS.Encoding/SFA.DAS.Encoding/EncodingService.cs
@@ -7,30 +7,30 @@ namespace SFA.DAS.Encoding
 {
     public class EncodingService : IEncodingService
     {
-        private readonly Dictionary<EncodingType, Hashids> _encodings;
+        private readonly Dictionary<EncodingType, Hashids> _hashids;
 
         public EncodingService(EncodingConfig config)
         {
-            _encodings = config.Encodings.ToDictionary(e => e.EncodingType, e => new Hashids(e.Salt, e.MinHashLength, e.Alphabet));
+            _hashids = config.Encodings.ToDictionary(e => e.EncodingType, e => new Hashids(e.Salt, e.MinHashLength, e.Alphabet));
         }
 
         public string Encode(long value, EncodingType encodingType)
         {
-            return _encodings[encodingType].EncodeLong(value);
+            return _hashids[encodingType].EncodeLong(value);
         }
 
         public long Decode(string value, EncodingType encodingType)
         {
             ValidateInput(value);
             
-            return _encodings[encodingType].DecodeLong(value)[0];
+            return _hashids[encodingType].DecodeLong(value)[0];
         }
 
         public bool TryDecode(string encodedValue, EncodingType encodingType, out long decodedValue)
         {
             ValidateInput(encodedValue);
             
-            var decodedValues = _encodings[encodingType].DecodeLong(encodedValue);
+            var decodedValues = _hashids[encodingType].DecodeLong(encodedValue);
             var isValid = decodedValues.Any();
             
             decodedValue = isValid ? decodedValues[0] : 0;
