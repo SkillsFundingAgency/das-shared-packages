1e4a02dd803e08b26a6020f158bfd7757267ea97
tom-gough <tom@tgsd.co.uk>
2019-05-28 11:56:55 +0100

    Added Active Directory project and classes

---
+++ SFA.DAS.ActiveDirectory/SFA.DAS.ActiveDirectory/IAzureAdAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.ActiveDirectory
+{
+    public interface IAzureAdAuthenticationService
+    {
+        Task<string> GetAuthenticationResult(string clientId, string appKey, string resourceId, string tenant);
+    }
+}
