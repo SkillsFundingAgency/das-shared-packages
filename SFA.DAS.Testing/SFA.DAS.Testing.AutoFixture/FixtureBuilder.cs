9c7b64d4de6999c63f65b83a83a97cd794e0062b
Scott Wakefield <scott@expertcc.co.uk>
2019-05-31 15:20:56 +0100

    MF-155 added additional attr for when using entity framework style entities.

---
+++ SFA.DAS.Testing/SFA.DAS.Testing.AutoFixture/FixtureBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace SFA.DAS.Testing.AutoFixture
+{
+    internal static class FixtureBuilder
+    {
+        public static IFixture MoqFixtureFactory()
+        {
+            var fixture = new Fixture();
+            fixture
+                .Customize(new AutoMoqCustomization{ConfigureMembers = true});
+
+            return fixture;
+        }
+
+        public static IFixture EFFriendlyMoqFixtureFactory()
+        {
+            var fixture = MoqFixtureFactory();
+
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
+        }
+    }
+}
--- SFA.DAS.Testing/SFA.DAS.Testing.AutoFixture/MoqAutoDataAttirbute.cs
+++ SFA.DAS.Testing/SFA.DAS.Testing.AutoFixture/MoqAutoDataAttirbute.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using AutoFixture;
-using AutoFixture.AutoMoq;
 using AutoFixture.NUnit3;
 
 namespace SFA.DAS.Testing.AutoFixture
 {
     public class MoqAutoDataAttribute : AutoDataAttribute
     {
-        public MoqAutoDataAttribute() : base(FixtureBuilder.FixtureFactory)
+        public MoqAutoDataAttribute() : base(FixtureBuilder.MoqFixtureFactory)
         {}
     }
 
@@ -15,20 +13,8 @@ namespace SFA.DAS.Testing.AutoFixture
     public class MoqInlineAutoDataAttribute : InlineAutoDataAttribute
     {
         public MoqInlineAutoDataAttribute(params object[] arguments)
-            : base(FixtureBuilder.FixtureFactory, arguments)
+            : base(FixtureBuilder.MoqFixtureFactory, arguments)
         {
-        }
-    }
-
-    internal static class FixtureBuilder
-    {
-        public static IFixture FixtureFactory()
-        {
-            var fixture = new Fixture();
-            fixture
-                .Customize(new AutoMoqCustomization{ConfigureMembers = true});
-
-            return fixture;
         }
     }
 }
