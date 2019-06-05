390e6a494c2507c7ac67501d8c1bd9590c9096ab
tom-gough <tom@tgsd.co.uk>
2019-06-05 08:59:21 +0100

    SFA.DAS.Binders

---
+++ SFA.DAS.Binders/SFA.DAS.Binders/TrimStringModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.Binders
+{
+    public sealed class TrimStringModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+            if (valueProviderResult?.AttemptedValue == null)
+            {
+                return null;
+            }
+
+            if (valueProviderResult.AttemptedValue == string.Empty)
+            {
+                return string.Empty;
+            }
+
+            return valueProviderResult.AttemptedValue.Trim();
+        }
+    }
+}
