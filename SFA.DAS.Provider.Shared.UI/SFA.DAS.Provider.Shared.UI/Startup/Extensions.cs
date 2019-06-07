bd3488b1fe567b4ba59cc219fcfcf7c76842e5fa
chrisfoster186 <chrisfoster186@googlemail.com>
2019-06-07 06:44:22 +0100

    Adds mvc builder extensions method for default nav section

---
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Startup/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Provider.Shared.UI.Attributes;
+
+namespace SFA.DAS.Provider.Shared.UI.Startup
+{
+    public static class Extensions
+    {
+        public static IMvcBuilder SetDefaultNavigationSection(this IMvcBuilder builder, NavigationSection defaultSection)
+        {
+            builder.Services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new SetNavigationSectionAttribute(defaultSection));
+            });
+            return builder;
+        }
+    }
+}
