e0696fe176278523920273d011713ac66a30f09b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-06-14 09:40:13 +0100

    Add example, package for NServiceBus in AzureFunc

---
+++ SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.AzureFunction/Infrastructure/EnvironmentVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.NServiceBus.AzureFunction.Infrastructure
+{
+    public class EnvironmentVariables
+    {
+        public static string NServiceBusConnectionString = Environment.GetEnvironmentVariable("NServiceBusConnectionString");
+        public static string NServiceBusLicense = Environment.GetEnvironmentVariable("NServiceBusLicense");
+    }
+}
---
+++ SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.AzureFunctionExample/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using SFA.DAS.NServiceBus.AzureFunction.Infrastructure;
+using SFA.DAS.NServiceBus.AzureFunctionExample;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace SFA.DAS.NServiceBus.AzureFunctionExample
+{
+    public class Startup :IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //Environment variables need creating for
+            //      NServiceBusConnectionString
+            //      NServiceBusLicense
+            builder.AddExecutionContextBinding();
+            builder.AddExtension<NServiceBusExtensionConfig>();
+        }
+    }
+}
