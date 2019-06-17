44683b2794eefa679d5c6232eeb83fbadd20ed93
tom-gough <tom@tgsd.co.uk>
2019-06-17 12:58:50 +0100

    Added test helper for configuration

---
+++ SFA.DAS.Testing/SFA.DAS.Testing/Helpers/ConfigurationTestHelper.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.AutoConfiguration;
+
+namespace SFA.DAS.Testing.Helpers
+{
+    public class ConfigurationTestHelper
+    {
+        public static T GetConfiguration<T>(string rowKey)
+        {
+            var configurationService = CreateConfigurationService();
+            return configurationService.Get<T>(rowKey);
+        }
+
+        private static IAutoConfigurationService CreateConfigurationService()
+        {
+            return new TableStorageConfigurationService(new EnvironmentService(), new AzureTableStorageConnectionAdapter());
+        }
+    }
+}
