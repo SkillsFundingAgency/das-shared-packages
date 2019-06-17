03637336dacb562fd4d2890814eec9aa1bf89fe0
tom-gough <tom@tgsd.co.uk>
2019-06-17 14:50:59 +0100

    Removed needless recreation of config service on every request

--- SFA.DAS.Testing/SFA.DAS.Testing/Helpers/ConfigurationTestHelper.cs
+++ SFA.DAS.Testing/SFA.DAS.Testing/Helpers/ConfigurationTestHelper.cs
@@ -4,10 +4,14 @@ namespace SFA.DAS.Testing.Helpers
 {
     public class ConfigurationTestHelper
     {
+        private static IAutoConfigurationService _configurationService;
+
         public static T GetConfiguration<T>(string rowKey)
         {
-            var configurationService = CreateConfigurationService();
-            return configurationService.Get<T>(rowKey);
+            if(_configurationService == null)
+                _configurationService = CreateConfigurationService();
+
+            return _configurationService.Get<T>(rowKey);
         }
 
         private static IAutoConfigurationService CreateConfigurationService()
