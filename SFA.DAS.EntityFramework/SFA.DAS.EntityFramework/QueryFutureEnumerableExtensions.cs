711090cfa800db9b6d71c7fdffe29a935278ea24
Michael Young <mick.young1981@googlemail.com>
2019-06-19 11:59:58 +0100

    Added new project for entity framework helpers taken from EAS

---
+++ SFA.DAS.EntityFramework/SFA.DAS.EntityFramework/QueryFutureEnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Z.EntityFramework.Plus;
+
+namespace SFA.DAS.EntityFramework
+{
+    public static class QueryFutureEnumerableExtensions
+    {
+        public static async Task<T> SingleOrDefaultAsync<T>(this QueryFutureEnumerable<T> source)
+        {
+            if (!source.HasValue)
+            {
+                await source.OwnerBatch.ExecuteQueriesAsync().ConfigureAwait(false);
+            }
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                {
+                    return default(T);
+                }
+
+                var current = enumerator.Current;
+
+                if (!enumerator.MoveNext())
+                {
+                    return current;
+                }
+            }
+
+            throw new InvalidOperationException("Sequence contains more than one element");
+        }
+
+        public static Task<T> ValueAsync<T>(this QueryFutureEnumerable<T> source) where T : struct
+        {
+            return source.SingleOrDefaultAsync();
+        }
+    }
+}
