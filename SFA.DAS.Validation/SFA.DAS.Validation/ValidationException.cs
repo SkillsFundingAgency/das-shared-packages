10a9cfb89a37610664edeb57623f4dbc60762d44
Paul Howes <paul.howes@cognetics.net>
2019-07-05 13:54:42 +0100

    Added extra method to ValidationException.

--- SFA.DAS.Validation/SFA.DAS.Validation/ValidationException.cs
+++ SFA.DAS.Validation/SFA.DAS.Validation/ValidationException.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace SFA.DAS.Validation
 {
+    public class ValidationException<T> : ValidationException
+    {
+        public ValidationException(Expression<Func<T, object>> expression, string message)
+            : base(typeof(T), ExpressionHelper.GetExpressionText(expression), message)
+        {
+        }
+    }
+
     public class ValidationException : Exception
     {
         public IEnumerable<ValidationError> ValidationErrors => _validationErrors;
 
         private readonly List<ValidationError> _validationErrors = new List<ValidationError>();
+
+        public Type MessageType { get; protected set; }
+        public string PropertyName { get; protected set; }
 
         public ValidationException()
             : base("")
@@ -18,6 +30,13 @@ namespace SFA.DAS.Validation
         public ValidationException(string message)
             : base(message)
         {
+        }
+
+        public ValidationException(Type messageType, string propertyName, string message)
+            : base(message)
+        {
+            MessageType = messageType;
+            PropertyName = propertyName;
         }
 
         public ValidationException AddError<T>(Expression<Func<T, object>> property, string message) where T : class
