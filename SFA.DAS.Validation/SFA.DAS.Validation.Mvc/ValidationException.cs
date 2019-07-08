84a4c2b70b2d06f5cccc7fdc1fc6ed44d33db139
Paul Howes <paul.howes@cognetics.net>
2019-07-08 14:20:47 +0100

    AML-3618 Added Validation to MVC package.

---
+++ SFA.DAS.Validation/SFA.DAS.Validation.Mvc/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿#if NET462
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Mvc;
+
+namespace SFA.DAS.Validation.Mvc
+{
+    public class ValidationException<T> : ValidationException
+    {
+        public ValidationException(Expression<Func<T, object>> expression, string message)
+            : base(typeof(T), ExpressionHelper.GetExpressionText(expression), message)
+        {
+        }
+    }
+}
+#endif
--- SFA.DAS.Validation/SFA.DAS.Validation/ValidationException.cs
+++ SFA.DAS.Validation/SFA.DAS.Validation/ValidationException.cs
@@ -10,6 +10,9 @@ namespace SFA.DAS.Validation
 
         private readonly List<ValidationError> _validationErrors = new List<ValidationError>();
 
+        public Type MessageType { get; protected set; }
+        public string PropertyName { get; protected set; }    
+
         public ValidationException()
             : base("")
         {
@@ -18,6 +21,13 @@ namespace SFA.DAS.Validation
         public ValidationException(string message)
             : base(message)
         {
+        }
+
+        public ValidationException(Type messageType, string propertyName, string message)
+            : base(message)
+        {
+            MessageType = messageType;
+            PropertyName = propertyName;
         }
 
         public ValidationException AddError<T>(Expression<Func<T, object>> property, string message) where T : class
