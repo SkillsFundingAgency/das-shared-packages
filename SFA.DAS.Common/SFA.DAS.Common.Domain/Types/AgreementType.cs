aa45d0b4c5865b75db12b97d9a89be65cb1fc036
Michael Young <mick.young1981@googlemail.com>
2019-07-29 11:05:20 +0100

    Renamed EoI enum value

--- SFA.DAS.Common/SFA.DAS.Common.Domain/Types/AgreementType.cs
+++ SFA.DAS.Common/SFA.DAS.Common.Domain/Types/AgreementType.cs
@@ -5,6 +5,6 @@ namespace SFA.DAS.Common.Domain.Types
     public enum AgreementType : byte
     {
         [Description("Levy")] Levy = 0,
-        [Description("Expression of Interest")] EoI = 1,
+        [Description("Expression of Interest")] NoneLevyExpressionOfInterest = 1,
     }
 }
