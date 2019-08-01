0feab9e05facbec8df7e84d45703731fe4efcec2
Paul Graham <paul.graham@coprime.co.uk>
2019-08-01 10:37:22 +0100

    Added SetNavigationSectionAttribute to allow us to show which section is active, defaults to None

---
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/NavigationSection.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.Employer.Shared.UI
+{
+    public enum NavigationSection
+    {
+        None,
+        AccountsHome,
+        AccountsFinance,
+        RecruitHome,
+        ApprenticesHome,
+        AccountsTeamsView,
+        AccountsAgreements,
+        AccountsSchemes
+    }
+}
