01275d67cbb4f96af4353ea8072f700d89398850
Lee Wadhams <lee.wadhams@valtech.com>
2019-08-02 15:31:57 +0100

    Add default value for link generation

--- SFA.DAS.EmployerUrlHelper/SFA.DAS.EmployerUrlHelper.UnitTests/LinkGeneratorTests.cs
+++ SFA.DAS.EmployerUrlHelper/SFA.DAS.EmployerUrlHelper.UnitTests/LinkGeneratorTests.cs
@@ -34,5 +34,34 @@ namespace SFA.DAS.EmployerUrlHelper.UnitTests
             Assert.AreEqual("https://reservations/path", linkGenerator.ReservationsLink("/path/"));
             Assert.AreEqual("https://users/path", linkGenerator.UsersLink("/path/"));
         }
+
+        [Test]
+        public void Link_WhenNoPathIsSpecified_ShouldReturnBaseUrl()
+        {
+            var employerUrlHelperConfiguration = new EmployerUrlHelperConfiguration
+            {
+                AccountsBaseUrl = "https://accounts/",
+                CommitmentsBaseUrl = "https://commitments/",
+                CommitmentsV2BaseUrl = "https://commitmentsv2/",
+                FinanceBaseUrl = "https://finance/",
+                PortalBaseUrl = "https://portal/",
+                ProjectionsBaseUrl = "https://projections/",
+                RecruitBaseUrl = "https://recruit/",
+                ReservationsBaseUrl = "https://reservations/",
+                UsersBaseUrl = "https://users/"
+            };
+
+            ILinkGenerator linkGenerator = new LinkGenerator(employerUrlHelperConfiguration);
+
+            Assert.AreEqual("https://accounts", linkGenerator.AccountsLink());
+            Assert.AreEqual("https://commitments", linkGenerator.CommitmentsLink());
+            Assert.AreEqual("https://commitmentsv2", linkGenerator.CommitmentsV2Link());
+            Assert.AreEqual("https://finance", linkGenerator.FinanceLink());
+            Assert.AreEqual("https://portal", linkGenerator.PortalLink());
+            Assert.AreEqual("https://projections", linkGenerator.ProjectionsLink());
+            Assert.AreEqual("https://recruit", linkGenerator.RecruitLink());
+            Assert.AreEqual("https://reservations", linkGenerator.ReservationsLink());
+            Assert.AreEqual("https://users", linkGenerator.UsersLink());
+        }
     }
 }
