c2357a2665aebbd16300b2b7f424bec2b3535a87
Elliot Ritchie <elliotritchie@hotmail.com>
2019-08-20 11:15:23 +0100

    Revert "Removed .NET Framework specific Azure Service Bus configuration" to prevent legacy .NET Framework consumers having to upgrade their Microsoft.IdentityModel.Clients.ActiveDirectory package right now
    This reverts commit fc9720c0ac32d2b5e12f1bef92446c9ea0d5a775.

--- SFA.DAS.NServiceBus/SFA.DAS.NServiceBus/AzureServiceBus/EndpointConfigurationExtensions.cs
+++ SFA.DAS.NServiceBus/SFA.DAS.NServiceBus/AzureServiceBus/EndpointConfigurationExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using NServiceBus;
+#if NET462
+using NServiceBus.Transport.AzureServiceBus;
+#endif
 
 namespace SFA.DAS.NServiceBus.AzureServiceBus
 {
@@ -7,14 +10,42 @@ namespace SFA.DAS.NServiceBus.AzureServiceBus
     {
         public static EndpointConfiguration UseAzureServiceBusTransport(this EndpointConfiguration config, string connectionString, Action<RoutingSettings> routing = null)
         {
-            var transport = config.UseTransport<AzureServiceBusTransport>();
-            var ruleNameShortener = new RuleNameShortener();
+#if NETSTANDARD2_0
+                var transport = config.UseTransport<AzureServiceBusTransport>();
+                var ruleNameShortener = new RuleNameShortener();
 
-            transport.ConnectionString(connectionString);
-            transport.RuleNameShortener(ruleNameShortener.Shorten);
-            transport.Transactions(TransportTransactionMode.ReceiveOnly);
+                transport.ConnectionString(connectionString);
+                transport.RuleNameShortener(ruleNameShortener.Shorten);
+                transport.Transactions(TransportTransactionMode.ReceiveOnly);
 
-            routing?.Invoke(transport.Routing());
+                routing?.Invoke(transport.Routing());
+#elif NET462
+                var transport = config.UseTransport<AzureServiceBusTransport>();
+
+                transport.BrokeredMessageBodyType(SupportedBrokeredMessageBodyTypes.Stream);
+                transport.ConnectionString(connectionString);
+                transport.Transactions(TransportTransactionMode.ReceiveOnly);
+                transport.UseForwardingTopology();
+
+                var messageReceivers = transport.MessageReceivers();
+
+                messageReceivers.AutoRenewTimeout(TimeSpan.FromMinutes(10));
+
+                var queues = transport.Queues();
+
+                queues.ForwardDeadLetteredMessagesTo(q => q != "error" && q != "audit" && q != "deadletter", "deadletter");
+                queues.LockDuration(TimeSpan.FromMinutes(1));
+
+                var subscriptions = transport.Subscriptions();
+
+                subscriptions.ForwardDeadLetteredMessagesTo("deadletters");
+
+                var sanitization = transport.Sanitization();
+
+                sanitization.UseStrategy<ValidateAndHashIfNeeded>();
+
+                routing?.Invoke(transport.Routing());
+#endif
 
             return config;
         }
