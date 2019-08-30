0144e63c7cf92b3184ebbad2d375497187b80c51
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-08-30 10:26:23 +0100

    ER-1428_New end point to return only live vacancy

--- SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client.UnitTests/ClientTests/GetVacancyTests.cs
+++ SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client.UnitTests/ClientTests/GetVacancyTests.cs
@@ -13,7 +13,20 @@ namespace SFA.DAS.Recruit.Vacancies.Client.UnitTests.ClientTests
             var sut = new Client(connectionString, "recruit", "queryStore", null);
 
             var vacancyReference = 1000000022;
-            var vac = sut.GetVacancy(vacancyReference);
+            var vac = sut.GetPublishedVacancy(vacancyReference);
+
+            Assert.AreEqual(vacancyReference, vac.VacancyReference);
+        }
+
+        [Ignore("A helper test to get live vacancy")]
+        [Test]
+        public void ShouldGetVacancyFromLiveView()
+        {
+            var connectionString = string.Empty;
+            var sut = new Client(connectionString, "recruit", "queryStore", null);
+
+            var vacancyReference = 1000005528;
+            var vac = sut.GetLiveVacancy(vacancyReference);
 
             Assert.AreEqual(vacancyReference, vac.VacancyReference);
         }
