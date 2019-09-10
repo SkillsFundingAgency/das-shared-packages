f2d9814173a101733302837b39ab62052c617846
Matt Derry <matt.derry@digital.education.gov.uk>
2019-09-10 12:19:22 +0100

    changes shared menu ui implementation to hide other menu options when section is none

--- SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/Configuration/MaPageConfiguration.cs
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/Configuration/MaPageConfiguration.cs
@@ -4,9 +4,6 @@ namespace SFA.DAS.Employer.Shared.UI.Configuration
 {
     public class MaPageConfiguration
     {
-
-        public bool ShowHomeOnly { get; set; } = false;
-
         public MaRoutes Routes { get; set; }
         public string AccountsOidcClientId { get; set; }
         public string LocalLogoutRouteName { get; set; }
