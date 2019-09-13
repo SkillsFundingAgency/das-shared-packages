26af2bc195ea492ab3b86202d84fca4cbe9a975f
Paul Howes <paul.howes@cognetics.net>
2019-09-13 12:14:36 +0100

    AML-3616 Migrate Unit Tests

---
+++ SFA.DAS.Hmrc/SFA.DAS.Hmrc.UnitTests/WhenIMakeABadRequest.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Hmrc.ExecutionPolicy;
+using SFA.DAS.Hmrc.Http;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.Hmrc.UnitTests
+{
+    public class WhenIMakeABadRequest
+    {
+        private Mock<ILog> _logger;
+        private HmrcExecutionPolicy _policy;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _logger = new Mock<ILog>();
+            _policy = new HmrcExecutionPolicy(_logger.Object);
+        }
+
+        [Test]
+        public void ThenIShouldNotRetryTheRequest()
+        {
+            //Arrange
+            Task TestAction(ref int callCount)
+            {
+                callCount++;
+                throw new HttpException((int) HttpStatusCode.BadRequest, "A bad request");
+            }
+
+            // Act
+            int actualNumberOfCalls = 0;
+            Assert.ThrowsAsync<HttpException>(() => _policy.ExecuteAsync(() => TestAction(ref actualNumberOfCalls)));
+
+            //Assert
+            const int expectedNumberOfCalls = 1;
+            Assert.AreEqual(expectedNumberOfCalls, actualNumberOfCalls);
+        }
+    }
+}
