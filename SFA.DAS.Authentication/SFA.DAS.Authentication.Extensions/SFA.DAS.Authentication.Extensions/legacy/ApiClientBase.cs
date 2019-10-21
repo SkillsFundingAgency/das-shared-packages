4f273dcc3e48811d897ef7a7a37f527ab11a75cf
Kevin <kevin@stephek.co.uk>
2019-10-21 15:42:58 +0100

    CON-829-namespace-change

--- SFA.DAS.Authentication/SFA.DAS.Authentication.Extensions/SFA.DAS.Authentication.Extensions/legacy/ApiClientBase.cs
+++ SFA.DAS.Authentication/SFA.DAS.Authentication.Extensions/SFA.DAS.Authentication.Extensions/legacy/ApiClientBase.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 
-namespace SFA.DAS.Http
+namespace SFA.DAS.Authentication.Extensions.Legacy
 {
     public abstract class ApiClientBase
     {
--- SFA.DAS.Authentication/SFA.DAS.Authentication.Extensions/SFA.DAS.Authentication.Extensions/legacy/QueryStringHelper.cs
+++ SFA.DAS.Authentication/SFA.DAS.Authentication.Extensions/SFA.DAS.Authentication.Extensions/legacy/QueryStringHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace SFA.DAS.Http
+namespace SFA.DAS.Authentication.Extensions.Legacy
 {
     internal sealed class QueryStringHelper
     {
