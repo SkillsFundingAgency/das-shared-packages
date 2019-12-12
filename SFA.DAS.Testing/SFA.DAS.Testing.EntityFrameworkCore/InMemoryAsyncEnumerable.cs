27beba6ee67ae2c0adebd2994004adbabc8db24b
Scott Wakefield <scott@expertcc.co.uk>
2019-12-12 12:09:31 +0000

    MF-405 added shared fake db for ef core testing.

---
+++ SFA.DAS.Testing/SFA.DAS.Testing.EntityFrameworkCore/InMemoryAsyncEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SFA.DAS.Testing.EntityFrameworkCore
+{
+    public class InMemoryAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public InMemoryAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        {
+        }
+
+        public InMemoryAsyncEnumerable(Expression expression)
+            : base(expression)
+        {
+        }
+
+        IQueryProvider IQueryable.Provider => new InMemoryAsyncQueryProvider<T>(this);
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new InMemoryDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        public IAsyncEnumerator<T> GetEnumerator()
+        {
+            return this.GetAsyncEnumerator();
+        }
+    }
+}
---
+++ SFA.DAS.Testing/SFA.DAS.Testing.EntityFrameworkCore/InMemoryDbAsyncEnumerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Testing.EntityFrameworkCore
+{
+    public class InMemoryDbAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> innerEnumerator;
+        private bool disposed = false;
+
+        public InMemoryDbAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            this.innerEnumerator = enumerator;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Task<bool> MoveNext(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(this.innerEnumerator.MoveNext());
+        }
+
+        public T Current => this.innerEnumerator.Current;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                    this.innerEnumerator.Dispose();
+                }
+
+                this.disposed = true;
+            }
+        }
+
+    }
+}
