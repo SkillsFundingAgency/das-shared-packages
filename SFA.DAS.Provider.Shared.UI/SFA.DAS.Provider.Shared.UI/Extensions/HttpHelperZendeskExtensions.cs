a3cccac7493e3a38930a402dbe72e174511dfdc8
Paul Graham <paul.graham@coprime.co.uk>
2019-12-13 13:34:58 +0000

    added Zendesk widget to provider layout page

---
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Extensions/HttpHelperZendeskExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SFA.DAS.Provider.Shared.UI.Extensions
+{
+    public static class HttpHelperZendeskExtensions
+    {
+        public static HtmlString SetZendeskSuggestion(this IHtmlHelper html, string suggestion)
+        {
+            return new HtmlString($"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ search: '{EscapeApostrophes(suggestion)}' }});</script>");
+        }
+
+        public static IHtmlContent SetZendeskLabels(this IHtmlHelper html, params string[] labels)
+        {
+            var apiCallString = "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
+
+            var first = true;
+            foreach (var label in labels)
+            {
+                if (!string.IsNullOrEmpty(label))
+                {
+                    if (!first) apiCallString += ",";
+                    first = false;
+
+                    apiCallString += $"'{ EscapeApostrophes(label) }'";
+                }
+            }
+
+            apiCallString += "] });</script>";
+
+            return new HtmlString(apiCallString);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+    }
+}
