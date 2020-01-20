104e3235ba1f058b080dc982c6e7db8a7f6dcbbe
Akshay Chugh <akshay.chugh30@outlook.com>
2020-01-20 18:12:39 +0000

    CV-543 Moved model and partial into shared package

---
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Models/GaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Provider.Shared.UI.Models
+{
+    public class GaData
+    {
+        public string DataLoaded { get; set; } = "dataLoaded";
+        public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
+        public string UkPrn { get; set; }
+        public string Vpv { get; set; }
+    }
+}
