827abb148cc1a2225bb5324356d260fa4a21f7e3
dannygb <dan.beavon@education.gov.uk>
2020-02-17 11:39:45 +0000

    Adds an integration test to ensure we can get results out of the index post ES 7.5 upgrade

---
+++ SFA.DAS.VacancyServices.Search/SFA.DAS.VacancyServices.Search.UnitTests/IntegrationTests/LocationSearchClientIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.VacancyServices.Search.UnitTests.IntegrationTests
+{
+    [TestFixture]
+    public class LocationSearchClientIntegrationTests
+    {
+        [Test]
+        [Category("integration")]
+        public async Task GetLocations()
+        {
+            var criteria = "cov";
+            var searchClient = new LocationSearchClient(new LocationSearchClientConfiguration()
+            {
+                HostName = "http://localhost:9200/",
+                Index = "local-faa-locations"
+            });
+
+            var result = searchClient.Search(criteria);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Locations);
+            Assert.IsTrue(result.Locations.Any());
+        }
+    }
+}
