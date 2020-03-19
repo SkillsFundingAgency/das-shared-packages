ed8498e974d618b5e283537934e55278c98f6887
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-03-19 12:08:37 +0000

    Add test website for testing the header and footer

---
+++ SFA.DAS.MA.Shared.UI/SFA.DAS.MA.Shared.UI.TestSite.Framework/Helpers/IConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.MA.Shared.UI.TestSite.Framework.Helpers
+{
+    public interface IConfiguration
+    {
+        string CdnBaseUrl { get; }
+        string EmployerAccountsBaseUrl { get; }
+        string EmployerCommitmentsBaseUrl { get; }
+        string EmployerFinanceBaseUrl { get; }
+        string EmployerRecruitBaseUrl { get; }
+        string EmployerPortalBaseUrl { get; }
+    }
+}
---
+++ SFA.DAS.MA.Shared.UI/SFA.DAS.MA.Shared.UI.TestSite.Framework/Helpers/IOidcConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.MA.Shared.UI.TestSite.Framework.Helpers
+{
+    interface IOidcConfiguration
+    {
+        string BaseAddress { get; }
+        string ClientId { get; }
+    }
+}
