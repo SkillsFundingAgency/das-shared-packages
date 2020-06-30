1c8caf920e5b2ea793733a288b2fc97f02bb5eaf
chrisfoster186 <chrisfoster186@googlemail.com>
2020-06-30 07:29:57 +0100

    Adds filter

---
+++ SFA.DAS.Validation/SFA.DAS.Validation.Mvc/Filters/PreserveModelStateFilter.cs
@@ -0,0 +1,24 @@
+﻿#if NETCOREAPP2_0
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Validation.Mvc.Extensions;
+
+namespace SFA.DAS.Validation.Mvc.Filters
+{
+    public class PreserveModelStateFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
+            var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
+            var serializableModelState = filterContext.ModelState.ToSerializable();
+
+            tempData.Set(serializableModelState);
+            filterContext.RouteData.Values.Merge(filterContext.HttpContext.Request.Query);
+            filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
+        }
+    }
+}
+#endif
