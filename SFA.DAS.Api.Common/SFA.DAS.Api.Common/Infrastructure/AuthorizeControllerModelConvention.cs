4fc860d73f0f390d611af35b4497c03042d39088
Scott Wakefield <scott@expertcc.co.uk>
2020-09-14 13:11:47 +0100

    FAT2-189 removed usage of policy names list.

--- SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AuthorizeControllerModelConvention.cs
+++ SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AuthorizeControllerModelConvention.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
@@ -6,9 +7,16 @@ namespace SFA.DAS.Api.Common.Infrastructure
 {
     public class AuthorizeControllerModelConvention : IControllerModelConvention
     {
+        private readonly IEnumerable<string> _policyNames;
+
+        public AuthorizeControllerModelConvention(IEnumerable<string> policyNames)
+        {
+            _policyNames = policyNames;
+        }
+
         public void Apply(ControllerModel controller)
         {
-            controller.Filters.Add(PolicyNames.PolicyNameList.FirstOrDefault(c=>c.Equals(controller.ControllerName)) != null
+            controller.Filters.Add(_policyNames.FirstOrDefault(c=>c.Equals(controller.ControllerName)) != null
                 ? new AuthorizeFilter(controller.ControllerName)
                 : new AuthorizeFilter(PolicyNames.Default));
         }
--- SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/PolicyNames.cs
+++ SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/PolicyNames.cs
@@ -1,15 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.Api.Common.Infrastructure
+﻿namespace SFA.DAS.Api.Common.Infrastructure
 {
-    public static class PolicyNames
+    public static partial class PolicyNames
     {
-        public static string DataLoad => nameof(DataLoad);
         public static string Default => nameof(Default);
-        
-        public static readonly List<string> PolicyNameList = new List<string>
-        {
-            DataLoad
-        };
+        public static string DataLoad => nameof(DataLoad);
     }
 }
