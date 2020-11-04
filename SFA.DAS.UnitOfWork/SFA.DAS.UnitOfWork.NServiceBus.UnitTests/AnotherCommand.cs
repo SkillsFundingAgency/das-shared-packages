8ce4a47b6fcdc35e616cf658e27e6a836081c5d2
Michael Young <mick.young1981@googlemail.com>
2020-11-04 15:44:47 +0000

    Attempt 2...use correct convention to identify commands.

---
+++ SFA.DAS.UnitOfWork/SFA.DAS.UnitOfWork.NServiceBus.UnitTests/AnotherCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NServiceBus;
+
+namespace SFA.DAS.UnitOfWork.NServiceBus.UnitTests
+{
+    public class AnotherCommand
+    {
+        public DateTime Created { get; }
+
+        public AnotherCommand(DateTime created)
+        {
+            Created = created;
+        }
+    }
+}
---
+++ SFA.DAS.UnitOfWork/SFA.DAS.UnitOfWork.NServiceBus/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using NServiceBus;
+
+namespace SFA.DAS.UnitOfWork.NServiceBus
+{
+    internal static class ObjectExtensions
+    {
+        internal static bool IsCommand(this object message)
+        {
+            return Regex.IsMatch(message.GetType().Name, "Command(V\\d+)?$") || message is ICommand;
+        }
+    }
+}
