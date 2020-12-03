d4a66c82c1b10f28d29584448f62a7229fec47b4
Paul Graham <paul.graham@coprime.co.uk>
2020-12-03 13:24:36 +0000

    Added support for IHttpClientBuilder by adding some extension methods

---
+++ SFA.DAS.Http/SFA.DAS.Http/IHttpClientBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Http.Configuration;
+using SFA.DAS.Http.MessageHandlers;
+using SFA.DAS.Http.TokenGenerators;
+
+namespace SFA.DAS.Http
+{
+    public static class IHttpClientBuilderExtensions
+    {
+        public static IHttpClientBuilder AddLogging(this IHttpClientBuilder httpBuilder)
+        {
+            httpBuilder.AddHttpMessageHandler(sp =>
+            {
+                var loggerFactory = sp.GetService<ILoggerFactory>();
+                return new LoggingMessageHandler(loggerFactory.CreateLogger<LoggingMessageHandler>());
+            });
+
+            return httpBuilder;
+        }
+
+        public static IHttpClientBuilder AddApimAuthorisationHeader(this IHttpClientBuilder httpBuilder, IApimClientConfiguration config)
+        {
+            httpBuilder.AddHttpMessageHandler(sp => new ApimHeadersHandler(config));
+            return httpBuilder;
+        }
+
+        public static IHttpClientBuilder AddDefaultHeaders(this IHttpClientBuilder httpBuilder)
+        {
+            httpBuilder.AddHttpMessageHandler(sp => new DefaultHeadersHandler());
+            return httpBuilder;
+        }
+
+        public static IHttpClientBuilder AddManagedIdentityAuthorisationHeader(this IHttpClientBuilder httpBuilder, IManagedIdentityTokenGenerator tokenGenerator)
+        {
+            httpBuilder.AddHttpMessageHandler(sp => new ManagedIdentityHeadersHandler(tokenGenerator));
+            return httpBuilder;
+        }
+    }
+}
