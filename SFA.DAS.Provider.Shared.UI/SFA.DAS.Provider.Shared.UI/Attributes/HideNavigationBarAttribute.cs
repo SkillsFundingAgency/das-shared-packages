6821c0788d02b696c894817eed5f07fc5a2d515f
Chris Woodcock <chris@humesoftware.com>
2021-03-05 12:13:41 +0000

    Allowed shared navigation bar to show account header but hide navigation links

--- SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Attributes/HideNavigationBarAttribute.cs
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Attributes/HideNavigationBarAttribute.cs
@@ -7,10 +7,34 @@ namespace SFA.DAS.Provider.Shared.UI.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class HideNavigationBarAttribute : ResultFilterAttribute
     {
+        private bool HideAccountHeader {get;}
+        private bool HideNavigationLinks {get;}
+
+        public HideNavigationBarAttribute()
+        {
+            HideAccountHeader = true;
+            HideNavigationLinks = true;
+        }
+        
+        public HideNavigationBarAttribute(bool hideAccountHeader, bool hideNavigationLinks)
+        {
+            HideAccountHeader = hideAccountHeader;
+            HideNavigationLinks = hideNavigationLinks;
+        }
+        
         public override void OnResultExecuting(ResultExecutingContext context)
         {
             if (!(context.Controller is Controller controller)) return;
-            controller.ViewData[ViewDataKeys.HideNavigationBar] = true;
+            
+            if (HideAccountHeader)
+            {
+                controller.ViewData[ViewDataKeys.HideAccountHeader] = true;
+            }
+
+            if(HideNavigationLinks)
+            {
+                controller.ViewData[ViewDataKeys.HideNavigationLinks] = true;
+            }
         }
     }
 }
--- SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/ViewDataKeys.cs
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/ViewDataKeys.cs
@@ -3,7 +3,8 @@
     public class ViewDataKeys
     {
         public const string SelectedNavigationSection = "SharedUI.SelectedNavigationSection";
-        public const string HideNavigationBar = "SharedUI.HiddenNavigationBar";
+        public const string HideNavigationLinks = "SharedUI.HiddenNavigationLinks";
+        public const string HideAccountHeader = "SharedUI.HiddenAccountHeader";
         public const string ShowBetaPhaseBanner = "SharedUI.ShowBetaPhaseBanner";
         public const string SuppressNavigationSection = "SharedUI.SuppressedNavigationSections";
         public const string EnableGoogleAnalytics = "SharedUI.EnableGoogleAnalytics";
