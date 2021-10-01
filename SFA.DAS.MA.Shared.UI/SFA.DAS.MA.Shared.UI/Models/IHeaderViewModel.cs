005aba120c72604c145f68c97ca4a61bfa5ecd20
Chris Woodcock <chris@humesoftware.com>
2021-10-01 16:12:57 +0100

    Remove hide or partially hide menu

--- SFA.DAS.MA.Shared.UI/SFA.DAS.MA.Shared.UI/Models/IHeaderViewModel.cs
+++ SFA.DAS.MA.Shared.UI/SFA.DAS.MA.Shared.UI/Models/IHeaderViewModel.cs
@@ -5,12 +5,10 @@ namespace SFA.DAS.MA.Shared.UI.Models
 {
     public interface IHeaderViewModel : ILinkCollection, ILinkHelper
     {
-        bool MenuIsPartial { get; }
         bool MenuIsHidden { get; }
-        bool NavBarIsHidden { get; }
         string SelectedMenu { get;}
         IUserContext UserContext { get; }
-        void HideMenu(bool partial = true);
+        void HideMenu();
         void SelectMenu(string menu);
         bool UseLegacyStyles { get; }
     }
