f2e4cc6f90967391c3a8a9f90ca1412a3cc0fbcd
Scott Wakefield <scott@expertcc.co.uk>
2022-08-25 13:50:03 +0100

    FAI-514 remove using.

--- SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client/Client.cs
+++ SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client/Client.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Authentication;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
