fad2c75ce9a88ac3fc06ab360439a78453e39232
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-09-12 07:24:20 +0100

    FAI-603 - Add services for Identity and JwtToken

---
+++ SFA.DAS.OidcMiddleware/SFA.DAS.OidcMiddleware.GovUk/Services/IJwtSecurityTokenService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SFA.DAS.OidcMiddleware.GovUk.Services
+{
+    public interface IJwtSecurityTokenService
+    {
+        string CreateToken(string clientId, string audience, ClaimsIdentity claimsIdentity,
+            SigningCredentials signingCredentials);
+    }
+}
---
+++ SFA.DAS.OidcMiddleware/SFA.DAS.OidcMiddleware.GovUk/Services/JwtSecurityTokenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SFA.DAS.OidcMiddleware.GovUk.Services
+{
+    public class JwtSecurityTokenService : IJwtSecurityTokenService
+    {
+        public string CreateToken(string clientId, string audience, ClaimsIdentity claimsIdentity,
+            SigningCredentials signingCredentials)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var value = handler.CreateJwtSecurityToken(clientId, audience, claimsIdentity, DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(5), DateTime.UtcNow, signingCredentials);
+
+            return value.RawData;
+        }
+    }
+}
