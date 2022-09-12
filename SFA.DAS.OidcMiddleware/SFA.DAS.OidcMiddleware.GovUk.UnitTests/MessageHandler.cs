b4cc398248403ddde3ca34363b1a4847b8fd0e9d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-09-12 08:15:42 +0100

    FAI-603 - Add OidcService and test project

---
+++ SFA.DAS.OidcMiddleware/SFA.DAS.OidcMiddleware.GovUk.UnitTests/MessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+
+namespace SFA.DAS.OidcMiddleware.GovUk.UnitTests
+{
+    public class MessageHandler
+    {
+        public static Mock<HttpMessageHandler> SetupMessageHandlerMock(HttpResponseMessage response, Uri baseUrl, HttpMethod httpMethod)
+        {
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+            httpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(c =>
+                        c.RequestUri != null
+                        && c.Method.Equals(httpMethod)
+                        && c.RequestUri.Equals(baseUrl)
+                    ),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync((HttpRequestMessage request, CancellationToken token) => response);
+            return httpMessageHandler;
+        }
+    }
+}
---
+++ SFA.DAS.OidcMiddleware/SFA.DAS.OidcMiddleware.GovUk/Services/IOidcService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using SFA.DAS.OidcMiddleware.GovUk.Models;
+
+namespace SFA.DAS.OidcMiddleware.GovUk.Services
+{
+    public interface IOidcService
+    {
+        Task<Token> GetToken(string code, string redirectUri);
+        Task PopulateAccountClaims(ClaimsIdentity claimsIdentity, string accessToken);
+    }
+}
