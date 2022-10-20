1ccc59e19f0674b543082edd12f1e162f4351c31
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-20 22:27:19 +0100

    FAI-630 - Add .netcore 2.2 sample site

---
+++ SFA.DAS.GovUK.Auth/SFA.DAS.GovUk.SampleSite.Legacy/AppStart/CustomClaims.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using SFA.DAS.GovUK.Auth.Services;
+
+namespace SFA.DAS.GovUK.SampleSite.AppStart
+{
+    public class CustomClaims : ICustomClaims
+    {
+        public async Task<IEnumerable<Claim>> GetClaims(TokenValidatedContext tokenValidatedContext)
+        {
+            var value = tokenValidatedContext?.Principal?.Identities.First().Claims
+                .FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier))
+                ?.Value;
+            return new List<Claim> {new Claim("EmployerAccountId", $"ABC123-{value}")};
+        }
+    }
+}
---
+++ SFA.DAS.GovUK.Auth/SFA.DAS.GovUk.SampleSite.Legacy/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFA.DAS.GovUk.SampleSite.Legacy.Views
+{
+    public class _ViewImports : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
