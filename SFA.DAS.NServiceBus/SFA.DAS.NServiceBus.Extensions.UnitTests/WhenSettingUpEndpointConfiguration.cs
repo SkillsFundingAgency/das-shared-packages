172382fda05e7bd02032c3e3dcfb842102172d5a
Paul Graham <paul.graham@coprime.co.uk>
2022-10-20 14:11:08 +0100

    chnaged tests to check some state

--- SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.Extensions.UnitTests/WhenSettingUpEndpointConfiguration.cs
+++ SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.Extensions.UnitTests/WhenSettingUpEndpointConfiguration.cs
@@ -1,6 +1,9 @@
-﻿using NServiceBus;
+﻿using FluentAssertions;
+using NServiceBus;
+using NServiceBus.Configuration.AdvancedExtensibility;
 using NUnit.Framework;
-
+using SFA.DAS.NServiceBus.Extensions.UnitTests.Messages.Commands;
+using SFA.DAS.NServiceBus.Extensions.UnitTests.Messages.Events;
 
 namespace SFA.DAS.NServiceBus.Extensions.UnitTests;
 
@@ -11,12 +14,16 @@ public class WhenSettingUpEndpointConfiguration
     {
         var sut = new EndpointConfiguration("test");
         sut.UseNewtonsoftJsonSerializer();
+        sut.GetSettings().Get<object>("MainSerializer").Should().NotBeNull();
     }
 
     [Test]
-    public void Then_UseMessageConventions_does_not_throw_error()
+    public void Then_UseMessageConventions_does_set_the_event_command_types()
     {
         var sut = new EndpointConfiguration("test");
         sut.UseMessageConventions();
+        var conventions = sut.Conventions();
+        conventions.Conventions.IsCommandType(typeof(TestCommand)).Should().BeTrue();
+        conventions.Conventions.IsEventType(typeof(TestEvent)).Should().BeTrue();
     }
 }
