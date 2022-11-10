7cab57ae2fd172923c010eb8bc5f336401b727f9
Paul Graham <paul.graham@coprime.co.uk>
2022-11-10 09:59:58 +0000

    remove template comment

--- SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.AzureFunction.Extensions/AutoSubscribe/ForceAutoEventSubscription.cs
+++ SFA.DAS.NServiceBus/SFA.DAS.NServiceBus.AzureFunction.Extensions/AutoSubscribe/ForceAutoEventSubscription.cs
@@ -4,13 +4,6 @@ using NServiceBus;
 
 namespace SFA.DAS.NServiceBus.AzureFunction.Extensions.AutoSubscribe;
 
-/*
- * 
-NOTE: These clases are added via the Nuget Package SFA.DAS.NServiceBus.AzureFunction.Extension so that we can automatically register the Subscription rules.
-These will only be registered in NServiceBus when a message is processed, so this sends a local message as a work round (until this issue is resolved)
- 
- */
-
 public class ForceAutoEventSubscription : IMessage { }
 
 public class ForceAutoEventSubscriptionFunction
