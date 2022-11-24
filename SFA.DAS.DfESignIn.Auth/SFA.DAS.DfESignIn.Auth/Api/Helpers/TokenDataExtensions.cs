55ef64ec75b7d3c2c474f864362cf061c20b730f
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2022-11-24 11:32:40 +0000

    Based PR Comments
    1) Renamed file TokenBuilderExtension to TokenDataExtensions
    2) Amended Exception messages to appropiate ones
    3) Included Unit Test cases to cover the logic under TokenDataExtensions

--- SFA.DAS.DfESignIn.Auth/SFA.DAS.DfESignIn.Auth/Api/Helpers/TokenDataExtensions.cs
+++ SFA.DAS.DfESignIn.Auth/SFA.DAS.DfESignIn.Auth/Api/Helpers/TokenDataExtensions.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace SFA.DAS.DfESignIn.Auth.Api.Helpers
 {
-    public static class TokenBuilderExtensions
+    public static class TokenDataExtensions
     {
         public static TokenBuilder ForAudience(this TokenBuilder tokenBuilder, string audience)
         {
-            if (string.IsNullOrWhiteSpace(audience)) throw new Exception();
+            if (string.IsNullOrWhiteSpace(audience)) throw new ArgumentNullException(nameof(audience));
             tokenBuilder.Audience = audience;
 
             tokenBuilder.TokenData.Payload.Add("aud", tokenBuilder.Audience);
@@ -18,7 +18,7 @@ namespace SFA.DAS.DfESignIn.Auth.Api.Helpers
 
         public static TokenBuilder UseAlgorithm(this TokenBuilder tokenBuilder, string algorithm)
         {
-            if (string.IsNullOrWhiteSpace(algorithm)) throw new Exception("Algorithm");
+            if (string.IsNullOrWhiteSpace(algorithm)) throw new ArgumentNullException(nameof(algorithm));
             tokenBuilder.Algorithm = algorithm;
 
             tokenBuilder.TokenData.Header.Add("alg", tokenBuilder.JsonWebAlgorithm.GetAlgorithm(algorithm));
@@ -28,7 +28,7 @@ namespace SFA.DAS.DfESignIn.Auth.Api.Helpers
 
         public static TokenBuilder WithSecretKey(this TokenBuilder tokenBuilder, string secret)
         {
-            if (string.IsNullOrWhiteSpace(secret)) throw new Exception();
+            if (string.IsNullOrWhiteSpace(secret)) throw new ArgumentNullException(nameof(secret));
             tokenBuilder.SecretKey = Encoding.UTF8.GetBytes(secret);
 
             return tokenBuilder;
@@ -36,7 +36,7 @@ namespace SFA.DAS.DfESignIn.Auth.Api.Helpers
 
         public static TokenBuilder Issuer(this TokenBuilder tokenBuilder, string issuer)
         {
-            if (string.IsNullOrWhiteSpace(issuer)) throw new Exception();
+            if (string.IsNullOrWhiteSpace(issuer)) throw new ArgumentNullException(nameof(issuer));
             tokenBuilder.Issuer = issuer;
 
             tokenBuilder.TokenData.Payload.Add("iss", issuer);
