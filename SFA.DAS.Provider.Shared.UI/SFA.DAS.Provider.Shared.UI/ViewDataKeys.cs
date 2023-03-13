99be5850e28fd42455c74f44841e893f0afa4c97
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-03-13 08:27:16 +0000

    FAI-809 - Remove attribute for csp

--- SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/ViewDataKeys.cs
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/ViewDataKeys.cs
@@ -10,7 +10,6 @@
         public const string EnableGoogleAnalytics = "SharedUI.EnableGoogleAnalytics";
         public const string ZenDeskConfiguration = "SharedUI.ZenDeskConfiguration";
         public const string EnableCookieBanner = "SharedUI.EnableCookieBanner";
-        public const string EnableCsp = "SharedUI.EnableCsp";
         public const string UseDfESignIn = "SharedUI.UseDfESignIn";
     }
 }
