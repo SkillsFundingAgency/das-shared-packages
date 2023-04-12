a43ab7d611a042c635d12186080e67a140d3f940
James King <hello@jamesmichaelking.com>
2023-04-12 21:05:59 +0100

    Cookie banner

---
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/Models/CookieBannerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Employer.Shared.UI.Models
+{
+    public class CookieBannerModel
+    {
+        public string AccountId { get; set; }
+
+    }
+}
