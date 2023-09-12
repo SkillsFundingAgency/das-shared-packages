4928cdcaa74aa15dab14df1d255409dde110ba5f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-09-12 07:28:07 +0100

    FAI-915 - Add client name enum

---
+++ SFA.DAS.DfESignIn.Auth/SFA.DAS.DfESignIn.Auth/Enums/ClientName.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+
+namespace SFA.DAS.DfESignIn.Auth.Enums
+{
+    public enum ClientName
+    {
+        [Description("providers|pas")]
+        ProviderRoatp = 1,
+        [Description("support-tools")]
+        BulkStop = 2,
+        [Description("identify-data-locks")]
+        DataLocks = 3,
+        [Description("review")]
+        Qa = 4,
+        [Description("admin")]
+        ServiceAdmin = 5,
+        [Description("console")]
+        SupportConsole = 6
+    }
+}
