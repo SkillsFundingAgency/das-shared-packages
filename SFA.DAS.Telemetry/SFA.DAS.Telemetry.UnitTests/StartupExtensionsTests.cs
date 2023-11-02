f63f75d920f8a0e91e03e74c4ea0286ad2911097
Deven Shah <devenshah1974@gmail.com>
2023-11-02 09:18:09 +0000

    Fixed test to have assert

--- SFA.DAS.Telemetry/SFA.DAS.Telemetry.UnitTests/StartupExtensionsTests.cs
+++ SFA.DAS.Telemetry/SFA.DAS.Telemetry.UnitTests/StartupExtensionsTests.cs
@@ -5,15 +5,16 @@ using SFA.DAS.Telemetry.RedactionService;
 using SFA.DAS.Telemetry.Startup;
 
 namespace SFA.DAS.Telemetry.UnitTests;
+
 public class StartupExtensionsTests
 {
     [Test]
     public void AddTelemetryUriRedaction_RegistersUriRedactionServiceWithCorrectKeys()
     {
-        Mock<IServiceCollection> serviceMock = new Mock<IServiceCollection>();
+        Mock<IServiceCollection> serviceMock = new();
 
         StartupExtensions.AddTelemetryUriRedaction(serviceMock.Object, "firstName, lastName");
 
-        serviceMock.Setup(s => s.Add(It.Is<ServiceDescriptor>(d => d.ServiceType == typeof(IUriRedactionService) && d.ImplementationInstance != null)));
+        serviceMock.Verify(s => s.Add(It.Is<ServiceDescriptor>(d => d.ServiceType == typeof(IUriRedactionService) && d.ImplementationFactory != null)));
     }
 }
