06e745c90fbf5fb105f81c77d3c789763e22c235
Deven Shah <devenshah1974@gmail.com>
2023-11-02 09:18:35 +0000

    Telemetry redaction options trims keys in the extension (#755)
    * Telemetry redaction options trims keys in the extension
    
    * Fixed test to have assert

--- SFA.DAS.Telemetry/SFA.DAS.Telemetry.UnitTests/RedactionServiceTests.cs
+++ SFA.DAS.Telemetry/SFA.DAS.Telemetry.UnitTests/RedactionServiceTests.cs
@@ -17,13 +17,14 @@ namespace SFA.DAS.Telemetry.UnitTests
         [TestCase("http://www.google.com?email=chris@private.com&isRobot=false", "http://www.google.com?email=REDACTED&isRobot=false")]
         [TestCase("http://www.google.com?isRobot=false&email=chris@private.com", "http://www.google.com?isRobot=false&email=REDACTED")]
         [TestCase("http://www.google.com?Email=chris@private.com", "http://www.google.com?Email=REDACTED")]
+        [TestCase("http://www.google.com?Email=chris@private.com&dateofBirth=2019-11-10", "http://www.google.com?Email=REDACTED&dateofBirth=REDACTED")]
         public void TestRedaction(string originalUri, string expectedUri)
         {
             var uri = new Uri(originalUri);
 
             var options = new UriRedactionOptions
             {
-                RedactionList = new List<string> { "email" }
+                RedactionList = new List<string> { "email", "dateOfBirth" }
             };
 
             var service = new UriRedactionService(options);
---
+++ SFA.DAS.Telemetry/SFA.DAS.Telemetry.UnitTests/StartupExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Telemetry.RedactionService;
+using SFA.DAS.Telemetry.Startup;
+
+namespace SFA.DAS.Telemetry.UnitTests;
+
+public class StartupExtensionsTests
+{
+    [Test]
+    public void AddTelemetryUriRedaction_RegistersUriRedactionServiceWithCorrectKeys()
+    {
+        Mock<IServiceCollection> serviceMock = new();
+
+        StartupExtensions.AddTelemetryUriRedaction(serviceMock.Object, "firstName, lastName");
+
+        serviceMock.Verify(s => s.Add(It.Is<ServiceDescriptor>(d => d.ServiceType == typeof(IUriRedactionService) && d.ImplementationFactory != null)));
+    }
+}
