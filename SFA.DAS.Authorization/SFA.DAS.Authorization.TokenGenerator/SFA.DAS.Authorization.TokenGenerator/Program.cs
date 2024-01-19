4a4f75a4fb3f59bd78506c6ed65734f050a0fc63
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2024-01-19 13:49:09 +0000

    FLP-570 Generate BearerToken

---
+++ SFA.DAS.Authorization/SFA.DAS.Authorization.TokenGenerator/SFA.DAS.Authorization.TokenGenerator/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SFA.DAS.Authorization.TokenGenerator.Models;
+using SFA.DAS.Authorization.TokenGenerator.Providers;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Text.Json;
+
+class program
+{
+	static void Main(string[] args)
+	{
+		DisplayTitle();
+
+		var appSettings = ConfigurationProvider.GetAppSettings();
+
+		List<Claim>? claims = null;
+
+		while (true)
+		{
+			//  Set User
+			if (claims == null)
+			{
+				claims = SetUserClaims(appSettings);
+				DisplayUser(claims);
+			}
+
+			DisplayInstructions();
+
+			var readKey = Console.ReadKey();
+
+			switch (readKey.Key)
+			{
+				case ConsoleKey.Escape:
+					Environment.Exit(0);
+					break;
+
+				case ConsoleKey.Spacebar:
+					claims = null;
+					break;
+
+				case ConsoleKey.Enter:
+					var token = BearerTokenProvider.GetBearerToken(appSettings.UserBearerTokenSigningKey, claims);
+					Console.WriteLine(token);
+					break;
+			}
+		}
+	}
+
+	static void DisplayTitle()
+	{
+		Console.WriteLine("======================================================================");
+		Console.WriteLine("                       Bearer Token Generator");
+		Console.WriteLine("======================================================================");
+	}
+
+	static void DisplayInstructions()
+	{
+		Console.WriteLine("");
+		Console.WriteLine("----------------------------------------------------------------------");
+		Console.WriteLine("                     PRESS");
+		Console.WriteLine("            Enter : To Generate New Token");
+		Console.WriteLine("            Space : To Select different user type");
+		Console.WriteLine("           Escape : To Exit");
+		Console.WriteLine("----------------------------------------------------------------------");
+		Console.WriteLine("");
+	}
+
+
+	static List<Claim> SetUserClaims(AppSettings appSettings)
+	{
+		var userType = string.Empty;
+
+		//  Select user type
+		while (userType != "P" && userType != "E")
+		{
+			Console.WriteLine("Enter P for provider user or E for employer user");
+			userType = Console.ReadLine()?.ToUpper();
+		}
+
+		var claims = UserProvider.GenerateUserClaims(userType, appSettings);
+		return claims;
+	}
+
+	static void DisplayUser(List<Claim> claims)
+	{
+		Console.WriteLine("");
+		Console.WriteLine("User Claims");
+		Console.WriteLine("");
+
+		var maxLabelLength = claims.Max(x => x.Type.Length) + 2; // 2 spaces to indent the line
+
+		foreach (var claim in claims)
+		{
+			var padding = new string(' ', maxLabelLength - claim.Type.Length);
+			Console.WriteLine($"{padding}{claim.Type} : {claim.Value}");
+		}
+		Console.WriteLine("");
+	}
+}
+
+
+
+
---
+++ SFA.DAS.Authorization/SFA.DAS.Authorization.TokenGenerator/SFA.DAS.Authorization.TokenGenerator/Providers/BearerTokenProvider.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SFA.DAS.Authorization.TokenGenerator.Providers;
+
+
+public static class BearerTokenProvider
+{
+    private static int _expiryTime = 5;
+
+    public static string GetBearerToken(string? signingKey, IEnumerable<Claim> claims)
+    {
+        ValidateSigningKey(signingKey);
+
+
+        var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(signingKey!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            signingCredentials: creds,
+            expires: DateTime.UtcNow.AddMinutes(_expiryTime)
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
+    }
+
+    private static void ValidateSigningKey(string? signingKey)
+    {
+        if (string.IsNullOrEmpty(signingKey))
+        {
+            throw new BearTokenException("Signing key cannot be null or empty");
+        }
+
+        const int minimumKeySize = 128;
+        if (signingKey.Length * 8 < minimumKeySize)
+        {
+            // This checks the key is at least 128 bits long, otherwise the token will fail to be generated
+            throw new BearTokenException("Signing key must exceed 128bits in length");
+        }
+    }
+
+    private static bool IsUserAuthenticated(ClaimsPrincipal? user)
+    {
+        if (user == null) return false;
+
+        if (user.Identity == null) return false;
+
+        return user.Identity.IsAuthenticated;
+    }
+}
+
+
+public class BearTokenException : Exception
+{
+    public BearTokenException(string message) : base(message)
+    {
+    }
+}
