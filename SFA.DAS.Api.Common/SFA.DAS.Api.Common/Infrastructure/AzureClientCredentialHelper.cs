ba1960f72168afc4c165dc5c44011adaa435a7f2
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-03-04 12:29:39 +0000

    Added VSCode & VSStudio options

--- SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
+++ SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
@@ -8,18 +8,30 @@ namespace SFA.DAS.Api.Common.Infrastructure
 {
     public class AzureClientCredentialHelper : IAzureClientCredentialHelper
     {
+        private const int MaxRetries = 2;
+        private readonly TimeSpan _networkTimeout = TimeSpan.FromSeconds(1);
+        private readonly TimeSpan _delay = TimeSpan.FromMilliseconds(100);
+
         public async Task<string> GetAccessTokenAsync(string identifier)
         {
             var azureServiceTokenProvider = new ChainedTokenCredential(
-                new ManagedIdentityCredential(options: new TokenCredentialOptions
-                {
-                    Retry = { NetworkTimeout = TimeSpan.FromSeconds(1), MaxRetries = 2, Delay = TimeSpan.FromMilliseconds(100) }
-                }),
-                new AzureCliCredential(options: new AzureCliCredentialOptions
-                { 
-                    Retry = { NetworkTimeout = TimeSpan.FromSeconds(1), MaxRetries = 2, Delay = TimeSpan.FromMilliseconds(100) }
-                }));
-            
+                    new ManagedIdentityCredential(options: new TokenCredentialOptions
+                    {
+                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay }
+                    }),
+                    new AzureCliCredential(options: new AzureCliCredentialOptions
+                    {
+                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay }
+                    }),
+                    new VisualStudioCredential(options: new VisualStudioCredentialOptions
+                    {
+                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay }
+                    }),
+                    new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions()
+                    {
+                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay }
+                    }));
+
             var accessToken = await azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes: new[] { identifier }));
 
             return accessToken.Token;
