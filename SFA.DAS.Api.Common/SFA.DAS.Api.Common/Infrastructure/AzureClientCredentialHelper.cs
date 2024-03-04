cf95181905af8a73e7e702090aa8ef6ec907fcfa
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-03-04 11:45:33 +0000

    Issue: APIM trying to get the Bearer token using ManagedIdentity is taking longer than expected. This issue could be happening only in the local/dev box.
    Error: The request was cancelled due to the configured HttpClient.Timeout of 100 seconds elapsing.
    
    Resolution: Setting default timeout for ManagedIdentityCredentials, If it can't get the token using ManagedIdentity try with AzureCLI.

--- SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
+++ SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using SFA.DAS.Api.Common.Interfaces;
@@ -10,10 +11,14 @@ namespace SFA.DAS.Api.Common.Infrastructure
         public async Task<string> GetAccessTokenAsync(string identifier)
         {
             var azureServiceTokenProvider = new ChainedTokenCredential(
-                new ManagedIdentityCredential(),
+                new ManagedIdentityCredential(options: new TokenCredentialOptions
+                {
+                    Retry = { NetworkTimeout = TimeSpan.FromSeconds(1), MaxRetries = 2, Delay = TimeSpan.FromMilliseconds(100) }
+                }),
                 new AzureCliCredential());
-            var accessToken = await azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes:new[]{identifier}));
-         
+            
+            var accessToken = await azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes: new[] { identifier }));
+
             return accessToken.Token;
         }
     }
