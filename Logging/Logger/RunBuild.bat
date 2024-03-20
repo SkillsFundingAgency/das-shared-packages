9089e4720162490de1fc609596f56a4593020e93
Chris Woodcock <chris@humesoftware.com>
2024-03-20 10:53:58 +0000

    Moved projects under single root and deleted old release definition

--- Logging/Logger/RunBuild.bat
+++
@@ -1,6 +0,0 @@
-"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "NUnit.Console" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "xunit.runner.console" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "PhantomJS" "-OutputDirectory" "tools" "-ExcludeVersion"
-
-"tools\FAKE\tools\FAKE.exe" "Build.fsx" %2 "publishDirectory=Publish" "buildMode=%1" "playList=%3"
