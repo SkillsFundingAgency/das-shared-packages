d80385761f47c839a6e88d278ca9194a94497ad8
Deven Shah <devenshah1974@gmail.com>
2024-05-15 10:49:12 +0100

    Added WithValues and CreateMany extensions

---
+++ SFA.DAS.Testing/SFA.DAS.Testing.AutoFixture.UnitTests/FixtureBuilderExtensions/WithValuesTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SFA.DAS.Testing.AutoFixture.UnitTests.FixtureBuilderExtensions;
+
+public class WithValuesTests
+{
+    [Test]
+    public void CreatesManyWithDistinctValues()
+    {
+        Fixture fixture = new();
+        int[] ids = [1, 5, 9];
+
+        var people = fixture.Build<Person>().WithValues(p => p.Id, ids).CreateMany(ids.Length);
+
+        people.Select(p => p.Id).Should().Equal(ids);
+    }
+
+    [Test]
+    public void CreatesManyFromGivenValues()
+    {
+        Fixture fixture = new();
+        string[] names = ["Tom", "Dick", "Harry"];
+
+        var people = fixture.Build<Person>().WithValues(p => p.Name, names).CreateMany(4);
+
+        people.Count().Should().Be(4);
+        people.Select(p => p.Name).Should().Contain(names);
+        people.Count(p => p.Name == "Tom").Should().Be(2);
+    }
+
+    [Test]
+    public void CreateOneWithFromGiveValues()
+    {
+        Fixture fixture = new();
+        string[] names = ["Tom", "Dick", "Harry"];
+
+        var person = fixture.Build<Person>().WithValues(p => p.Name, names).Create();
+
+        person.Name.Should().Be("Tom");
+    }
+
+    public record Person(int Id, string Name);
+}
---
+++ SFA.DAS.Testing/SFA.DAS.Testing.AutoFixture/FixtureBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using AutoFixture;
+using AutoFixture.Dsl;
+using AutoFixture.Kernel;
+
+namespace SFA.DAS.Testing.AutoFixture;
+public static class FixtureBuilderExtensions
+{
+    /// <summary>
+    /// Builds a property from the given collection of "values". 
+    /// 
+    /// When creating many if the length is greater than count of "values" then "values" are recycled
+    /// </summary>
+    public static IPostprocessComposer<T> WithValues<T, TProperty>(
+        this IPostprocessComposer<T> composer,
+        Expression<Func<T, TProperty>> property,
+        params TProperty[] values)
+    {
+        var queue = new Queue<TProperty>(values);
+
+        return composer.With(property, () =>
+        {
+            if (queue.Count == 0) queue = new Queue<TProperty>(values);
+            return queue.Dequeue();
+        });
+    }
+
+    /// <summary>
+    /// Creates as many objects as many of given "values".
+    /// 
+    /// For each object created, builds the "property" from the given values
+    /// </summary>
+    public static IEnumerable<T> CreateMany<T, TProperty>(
+        this IPostprocessComposer<T> composer,
+        Expression<Func<T, TProperty>> property,
+        params TProperty[] values)
+    {
+        var queue = new Queue<TProperty>(values);
+
+        ISpecimenBuilder builder = composer.With(property, () => queue.Dequeue());
+
+        return builder.CreateMany<T>(values.Length);
+    }
+}
