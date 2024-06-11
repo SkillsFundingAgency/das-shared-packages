9026dfc4dc63c9fe06be3f164ddd59b8132e8d1e
tom-gough <tom.gough.tgsd@outlook.com>
2024-06-11 11:44:26 +0100

    FLP-823 Added Banner flags for apprentice details

---
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Models/Flags/ApprenticeDetailsBanners.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SFA.DAS.Provider.Shared.UI.Models.Flags;
+
+/// <summary>
+/// Flags for displaying banners on the apprentice details page. Note that these are bit flags and should be powers of 2.
+/// </summary>
+[Flags]
+public enum ApprenticeDetailsBanners : ulong
+{
+    None = 0,
+    ChangeOfStartDateSent = 1,
+    ChangeOfStartDateApproved = 2,
+    ChangeOfStartDateCancelled = 4,
+    ChangeOfPriceRequestSent = 8,
+    PriceChangeCancelled = 16,
+    PriceChangeApproved = 32,
+    ChangeOfPriceAutoApproved = 64,
+    PriceChangeRejected = 128
+}
