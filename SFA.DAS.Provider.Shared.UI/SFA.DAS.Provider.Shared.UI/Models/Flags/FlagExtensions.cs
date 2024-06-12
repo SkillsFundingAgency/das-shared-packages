8a86d74c26a1d772c0a5a75a0ff28bb077343971
tom-gough <tom.gough.tgsd@outlook.com>
2024-06-12 16:03:15 +0100

    FLP-823 shared package update

---
+++ SFA.DAS.Provider.Shared.UI/SFA.DAS.Provider.Shared.UI/Models/Flags/FlagExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.Provider.Shared.UI.Models.Flags
+{
+    public static class FlagExtensions
+    {
+        public static bool IsFlagSet(this ApprenticeDetailsBanners banners, ApprenticeDetailsBanners flag)
+        {
+            return (banners & flag) == flag;
+        }
+    }
+}
