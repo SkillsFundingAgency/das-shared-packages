e143506ce9e354a92840032110c10ddbbb88c12f
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-11-04 16:19:34 +0000

    FAI-2049 - Update to model

--- SFA.DAS.GovUK.Auth/SFA.DAS.GovUK.Auth/Employer/EmployerUserAccounts.cs
+++ SFA.DAS.GovUK.Auth/SFA.DAS.GovUK.Auth/Employer/EmployerUserAccounts.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace SFA.DAS.GovUK.Auth.Employer;
 
@@ -15,4 +16,13 @@ public class EmployerUserAccountItem
 {
     public string AccountId { get; set; }
     public string Role { get; set; }
+    public string EmployerName { get; set; }
+    public ApprenticeshipEmployerType ApprenticeshipEmployerType { get; set; }
+}
+
+public enum ApprenticeshipEmployerType : byte
+{
+    [Description("Non Levy")] NonLevy,
+    [Description("Levy")] Levy,
+    [Description("Unknown")] Unknown,
 }
