33ce2ee3667ad02c20eb60647849d17f40f5c634
Jon Harris <jon.harris@fuitsu.com>
2025-02-25 13:16:34 +0000

    When filter.OnActionExecuting() is called and the modelstate is not valid then only add the modelstate entities in error to the temp data

--- SFA.DAS.Validation/SFA.DAS.Validation.Mvc/Filters/ValidateModelStateFilter.cs
+++ SFA.DAS.Validation/SFA.DAS.Validation.Mvc/Filters/ValidateModelStateFilter.cs
@@ -6,67 +6,73 @@ using SFA.DAS.Validation.Exceptions;
 using SFA.DAS.Validation.Mvc.Extensions;
 using SFA.DAS.Validation.Mvc.ModelBinding;
 
-namespace SFA.DAS.Validation.Mvc.Filters
-{
-    public class ValidateModelStateFilter : ActionFilterAttribute
-    {
-        private static readonly string ModelStateKey = typeof(SerializableModelStateDictionary).FullName;
+namespace SFA.DAS.Validation.Mvc.Filters;
 
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+public class ValidateModelStateFilter : ActionFilterAttribute
+{
+    private static readonly string ModelStateKey = typeof(SerializableModelStateDictionary).FullName;
+
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        if (filterContext.HttpContext.Request.Method == "GET")
         {
-            if (filterContext.HttpContext.Request.Method == "GET")
+            if (!filterContext.ModelState.IsValid)
             {
-                if (!filterContext.ModelState.IsValid)
-                {
-                    filterContext.Result = new BadRequestObjectResult(filterContext.ModelState);
-                }
-                else
-                {
-                    var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
-                    var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
-                    var serializableModelState = tempData.Get<SerializableModelStateDictionary>();
-                    var modelState = serializableModelState?.ToModelState();
+                filterContext.Result = new BadRequestObjectResult(filterContext.ModelState);
+            }
+            else
+            {
+                var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
+                var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
+                var serializableModelState = tempData.Get<SerializableModelStateDictionary>();
+                var modelState = serializableModelState?.ToModelState();
                     
-                    filterContext.ModelState.Merge(modelState);
-                }
+                filterContext.ModelState.Merge(modelState);
+            }
+        }
+        else if (!filterContext.ModelState.IsValid)
+        {
+            var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
+            var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
+                
+            // Serializing the entire model state has caused issues with cookie bloat causing 400 Bad Request responses
+            // when submitting a form containing a larger payload which subsequently fails validation.
+            
+            // By only persisting any state which is in error to the temp-data (cookies) works around this issue.
+            // An example https://skillsfundingagency.atlassian.net/browse/CON-5373
+            
+            var serializableModelState = filterContext.ModelState.ToSerializableWithOnlyErrors();
+            
+            tempData.Set(serializableModelState);
+            filterContext.RouteData.Values.Merge(filterContext.HttpContext.Request.Query);
+            filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
+        }
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext filterContext)
+    {
+        if (filterContext.HttpContext.Request.Method != "GET")
+        {
+            if (filterContext.Exception is ValidationException validationException)
+            {
+                filterContext.ModelState.AddModelError(validationException);
+                    
+                var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
+                var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
+                var serializableModelState = filterContext.ModelState.ToSerializable();
+                    
+                tempData.Set(serializableModelState);
+                filterContext.RouteData.Values.Merge(filterContext.HttpContext.Request.Query);
+                filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
+                filterContext.ExceptionHandled = true;
             }
             else if (!filterContext.ModelState.IsValid)
             {
                 var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
                 var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
                 var serializableModelState = filterContext.ModelState.ToSerializable();
-                
+                    
                 tempData.Set(serializableModelState);
-                filterContext.RouteData.Values.Merge(filterContext.HttpContext.Request.Query);
-                filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
-            }
-        }
-
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            if (filterContext.HttpContext.Request.Method != "GET")
-            {
-                if (filterContext.Exception is ValidationException validationException)
-                {
-                    filterContext.ModelState.AddModelError(validationException);
-                    
-                    var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
-                    var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
-                    var serializableModelState = filterContext.ModelState.ToSerializable();
-                    
-                    tempData.Set(serializableModelState);
-                    filterContext.RouteData.Values.Merge(filterContext.HttpContext.Request.Query);
-                    filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
-                    filterContext.ExceptionHandled = true;
-                }
-                else if (!filterContext.ModelState.IsValid)
-                {
-                    var tempDataFactory = filterContext.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
-                    var tempData = tempDataFactory.GetTempData(filterContext.HttpContext);
-                    var serializableModelState = filterContext.ModelState.ToSerializable();
-                    
-                    tempData.Set(serializableModelState);
-                }
             }
         }
     }
