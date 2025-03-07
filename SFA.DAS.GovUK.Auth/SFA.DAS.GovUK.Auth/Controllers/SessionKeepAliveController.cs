a75a756418d10b7d9a443531c1ca380d9e68ed7c
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-03-07 09:49:39 +0000

    remove extension method

--- SFA.DAS.GovUK.Auth/SFA.DAS.GovUK.Auth/Controllers/SessionKeepAliveController.cs
+++ SFA.DAS.GovUK.Auth/SFA.DAS.GovUK.Auth/Controllers/SessionKeepAliveController.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.GovUK.Auth.Controllers;
 
 [Route("service/keepalive")]
-[Authorize]
 public class SessionKeepAliveController : Controller
 {
     [HttpGet]
