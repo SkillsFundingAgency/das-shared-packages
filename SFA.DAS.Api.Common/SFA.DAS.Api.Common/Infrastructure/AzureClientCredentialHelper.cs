cf4632434217e4e5e0f57236806adeec12424da6
Jon Harris <jon.harris@fuitsu.com>
2025-03-10 08:59:57 +0000

    Refactoring ChainedTokenCredential to a field to take advantage of the built in caching it provides

--- SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
+++ SFA.DAS.Api.Common/SFA.DAS.Api.Common/Infrastructure/AzureClientCredentialHelper.cs
@@ -5,71 +5,76 @@ using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using SFA.DAS.Api.Common.Interfaces;
 
-namespace SFA.DAS.Api.Common.Infrastructure
+namespace SFA.DAS.Api.Common.Infrastructure;
+
+public class AzureClientCredentialHelper : IAzureClientCredentialHelper
 {
-    public class AzureClientCredentialHelper : IAzureClientCredentialHelper
+    private const int MaxRetries = 2;
+    private readonly TimeSpan _networkTimeout = TimeSpan.FromMilliseconds(500);
+    private readonly TimeSpan _delay = TimeSpan.FromMilliseconds(100);
+
+    // Take advantage of built-in token caching.
+    // https://learn.microsoft.com/en-us/dotnet/api/azure.identity.chainedtokencredential?view=azure-dotnet
+    private ChainedTokenCredential _chainedTokenCredential;
+
+    [Obsolete("Use AzureClientCredentialHelper(IConfiguration) instead")]
+    public AzureClientCredentialHelper()
     {
-        private const int MaxRetries = 2;
-        private readonly TimeSpan _networkTimeout = TimeSpan.FromMilliseconds(500);
-        private readonly TimeSpan _delay = TimeSpan.FromMilliseconds(100);
-        private readonly bool _isLocal;
+        InitializeChainedTokenCredential(false);
+    }
 
-        [Obsolete("Use AzureClientCredentialHelper(IConfiguration) instead")]
-        public AzureClientCredentialHelper()
-        {
-            _isLocal = false;
-        }
-        
-        public AzureClientCredentialHelper(IConfiguration configuration)
-        {
-            var resourceEnvironmentName = configuration["ResourceEnvironmentName"];
-            _isLocal = resourceEnvironmentName != null && resourceEnvironmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase);
-        }
-        
-        public async Task<string> GetAccessTokenAsync(string identifier)
-        {
-            ChainedTokenCredential azureServiceTokenProvider;
-            if (_isLocal)
-            {
-                azureServiceTokenProvider = new ChainedTokenCredential(
-                    new AzureCliCredential(options: new AzureCliCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }),
-                    new VisualStudioCredential(options: new VisualStudioCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }),
-                    new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }));   
-                
-            }
-            else
-            {
-                azureServiceTokenProvider = new ChainedTokenCredential(
-                    new ManagedIdentityCredential(options: new TokenCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }),
-                    new AzureCliCredential(options: new AzureCliCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }),
-                    new VisualStudioCredential(options: new VisualStudioCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }),
-                    new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions
-                    {
-                        Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
-                    }));
-            }
-            
-            var accessToken = await azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes: [identifier]));
+    public AzureClientCredentialHelper(IConfiguration configuration)
+    {
+        var resourceEnvironmentName = configuration["ResourceEnvironmentName"];
+        var isLocal = resourceEnvironmentName != null && resourceEnvironmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase);
 
-            return accessToken.Token;
+        InitializeChainedTokenCredential(isLocal);
+    }
+
+    private void InitializeChainedTokenCredential(bool isLocal)
+    {
+        if (isLocal)
+        {
+            _chainedTokenCredential = new ChainedTokenCredential(
+                new AzureCliCredential(options: new AzureCliCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }),
+                new VisualStudioCredential(options: new VisualStudioCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }),
+                new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }));
         }
+        else
+        {
+            _chainedTokenCredential = new ChainedTokenCredential(
+                new ManagedIdentityCredential(options: new TokenCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }),
+                new AzureCliCredential(options: new AzureCliCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }),
+                new VisualStudioCredential(options: new VisualStudioCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }),
+                new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions
+                {
+                    Retry = { NetworkTimeout = _networkTimeout, MaxRetries = MaxRetries, Delay = _delay, Mode = RetryMode.Fixed }
+                }));
+        }
+    }
+
+    public async Task<string> GetAccessTokenAsync(string identifier)
+    {
+        var accessToken = await _chainedTokenCredential.GetTokenAsync(new TokenRequestContext(scopes: [identifier]));
+
+        return accessToken.Token;
     }
 }
