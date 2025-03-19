42fb66cdbfa33cd4972f40fbc84011cfb9c40193
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-03-19 12:22:27 +0000

    add endpoint

---
+++ SFA.DAS.DfESignIn.Auth/SFA.DAS.DfESignIn.Auth/Controllers/SessionKeepAliveController.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.DfESignIn.Auth.Controllers
+{
+    [Route("service/keepalive")]
+    public class SessionKeepAliveController() : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+           return NoContent();
+        }
+    }
+} 
