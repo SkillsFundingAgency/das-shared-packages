077d01546de5cf53e6d159428647acbc00374f79
tom-gough <tom.gough.tgsd@outlook.com>
2025-05-20 12:16:11 +0100

    FLP-411 fixes

--- SFA.DAS.Testing/SFA.DAS.Testing/PackageScanning/PackageScanResult.cs
+++ SFA.DAS.Testing/SFA.DAS.Testing/PackageScanning/PackageScanResult.cs
@@ -4,6 +4,6 @@ namespace SFA.DAS.Testing.PackageScanning;
 
 public class PackageScanResult
 {
-    public List<string> VulnerablePackageResults { get; set; }
-    public List<string> DeprecatedPackageResults { get; set; }
+    public List<string> VulnerablePackageResults { get; set; } = [];
+    public List<string> DeprecatedPackageResults { get; set; } = [];
 }
