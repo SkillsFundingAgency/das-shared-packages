0fdc0d557e00a1cc993fa39008a4026929ef5962
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-06-02 14:14:32 +0100

    ✨ Add VacancyReference JSON converter and related updates
    - Implemented `VacancyReferenceJsonConverter` for JSON serialization.
    - Created `InvalidVacancyReferenceException` for error handling.
    - Enhanced `VacancyReference` struct with new methods and operators.
    - Added unit tests for `VacancyReference` functionality and JSON handling.
    - Updated project files for `Newtonsoft.Json` and Visual Studio compatibility.
    
    Changes made by Balaji Jambulingam

---
+++ SFA.DAS.Common/SFA.DAS.Common.Domain/Json/VacancyReferenceJsonConverter.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using SFA.DAS.Common.Domain.Models;
+using System;
+
+namespace SFA.DAS.Common.Domain.Json
+{
+    public class VacancyReferenceJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(VacancyReference);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Null:
+                    return VacancyReference.None;
+                case JsonToken.String:
+                    return new VacancyReference((string)reader.Value);
+                case JsonToken.Integer:
+                    return new VacancyReference((long)(reader.Value ?? 0));
+                case JsonToken.None:
+                case JsonToken.StartObject:
+                case JsonToken.StartArray:
+                case JsonToken.StartConstructor:
+                case JsonToken.PropertyName:
+                case JsonToken.Comment:
+                case JsonToken.Raw:
+                case JsonToken.Float:
+                case JsonToken.Boolean:
+                case JsonToken.Undefined:
+                case JsonToken.EndObject:
+                case JsonToken.EndArray:
+                case JsonToken.EndConstructor:
+                case JsonToken.Date:
+                case JsonToken.Bytes:
+                default:
+                    return VacancyReference.None;
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value != null)
+            {
+                var reference = (VacancyReference)value;
+                if (VacancyReference.None.Equals(reference))
+                    writer.WriteNull();
+                else
+                    writer.WriteValue(reference.ToString());
+            }
+        }
+    }
+}
---
+++ SFA.DAS.Common/SFA.DAS.Common.Domain/Models/VacancyReference.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using SFA.DAS.Common.Domain.Json;
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace SFA.DAS.Common.Domain.Models
+{
+    public class InvalidVacancyReferenceException(string vacancyReference) : Exception($"The value '{vacancyReference}' is not a valid Vacancy Reference");
+
+    [JsonConverter(typeof(VacancyReferenceJsonConverter))]
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct VacancyReference : IEquatable<VacancyReference>,
+        IEquatable<string>,
+        IEquatable<long>
+    {
+        private bool IsNone => Value == 0;
+
+        public static readonly VacancyReference None = new(string.Empty);
+        public long Value { get; } = 0L;
+
+        public VacancyReference(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+
+            string baseValue = value.ToUpper().Replace("VAC", null);
+            if (!long.TryParse(baseValue, out long result) || result < 1)
+            {
+                throw new InvalidVacancyReferenceException(value);
+            }
+
+            Value = result;
+        }
+
+        public VacancyReference(long? vacancyReference)
+        {
+            switch (vacancyReference)
+            {
+                case null:
+                    return;
+                case < 1:
+                    throw new InvalidVacancyReferenceException($"{vacancyReference}");
+                default:
+                    Value = vacancyReference.Value;
+                    break;
+            }
+        }
+
+        public static bool operator ==(VacancyReference? left, VacancyReference? right)
+        {
+            if (left is null && right is null)
+            {
+                return true;
+            }
+
+            return left?.Equals(right) ?? right!.Value.Equals(left);
+        }
+
+        public static bool operator !=(VacancyReference? left, VacancyReference? right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj switch
+            {
+                null when IsNone => true,
+                null => false,
+                VacancyReference vacancyReference => Value == vacancyReference.Value,
+                long value => value == Value,
+                string value => IsNone && (this.ToString() == value || this.ToShortString() == value),
+                _ => false
+            };
+        }
+
+        public bool Equals(VacancyReference other)
+        {
+            return Value == other.Value;
+        }
+
+        public bool Equals(long other)
+        {
+            return Value == other;
+        }
+
+        public bool Equals(string? other)
+        {
+            return IsNone && other is null || (this.ToString() == other || this.ToShortString() == other);
+        }
+
+        public override string ToString()
+        {
+            return this == None
+                ? string.Empty
+                : $"VAC{Value}";
+        }
+
+        public string ToShortString()
+        {
+            return this == None
+                ? string.Empty
+                : $"{Value}";
+        }
+
+        public static implicit operator VacancyReference(long? value)
+        {
+            return value is null ? None : new VacancyReference(value.Value);
+        }
+
+        public static implicit operator VacancyReference(string? value)
+        {
+            return value is null ? None : new VacancyReference(value);
+        }
+
+        public static VacancyReference Parse(string value, IFormatProvider? provider)
+        {
+            if (TryParse(value, provider, out var result))
+            {
+                return result;
+            }
+
+            throw new InvalidVacancyReferenceException(value);
+        }
+
+        public static bool TryParse(string? value, IFormatProvider? provider, out VacancyReference result)
+        {
+            if (value is null)
+            {
+                result = None;
+                return true;
+            }
+
+            try
+            {
+                result = new VacancyReference(value);
+                return true;
+            }
+            catch
+            {
+                result = None;
+                return false;
+            }
+        }
+
+        public static bool TryParse(string? value, out VacancyReference result)
+        {
+            return TryParse(value, CultureInfo.CurrentCulture, out result);
+        }
+    }
+}
