696bc2daab4bc9740174568fe8108a71d9d9a5c5
furqanagwan <furqanagwan@outlook.com>
2026-01-08 11:11:20 +0000

    Test this Net9 upgrade

--- SFA.DAS.ApiTokens/SFA.DAS.ApiTokens.Lib/IJwtTokenService.cs
+++ SFA.DAS.ApiTokens/SFA.DAS.ApiTokens.Lib/IJwtTokenService.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
+namespace SFA.DAS.ApiTokens.Lib;
 
-namespace SFA.DAS.ApiTokens.Lib
+public interface IJwtTokenService
 {
-    public interface IJwtTokenService
-    {
-        string Encode(string data, string audience, string issuer, double lifetimeDurationSeconds = 60);
-        string Decode(string token, IEnumerable<string> validAudiences, IEnumerable<string> validIssuers);
-    }
+    string Encode(string data, string audience, string issuer, double lifetimeDurationSeconds = 60);
+    string Decode(string token, IEnumerable<string> validAudiences, IEnumerable<string> validIssuers);
 }
