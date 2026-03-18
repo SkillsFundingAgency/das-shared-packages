000645efa852a41b0f97afbc98b899927d7fb598
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-03-18 20:04:39 +0000

    delete more

--- SFA.DAS.Apim.Shared/SFA.DAS.Apim.Shared/Common/Constants.cs
+++ SFA.DAS.Apim.Shared/SFA.DAS.Apim.Shared/Common/Constants.cs
@@ -1,13 +1,5 @@
 ﻿namespace SFA.DAS.Apim.Shared.Common;
 
-public static class Constants
-{
-    /// <summary>
-    /// number of days
-    /// </summary>
-    public const int QualifyingPeriod = 42;
-}
-
 public static class ApiVersionNumber
 {
     public const string One = "1.0";
