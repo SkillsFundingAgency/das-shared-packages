41fd9e5f87c2304eacabdfd10e5dcf20f5da04e6
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-04-16 19:42:56 +0100

    FAI-3118 - Allow you to override the configuration name for the client
    Also on the comments show the return type

--- SFA.DAS.ApiContracts.Build/SFA.DAS.ApiContracts.Build.Tests/Generated/Responses.g.cs
+++ SFA.DAS.ApiContracts.Build/SFA.DAS.ApiContracts.Build.Tests/Generated/Responses.g.cs
@@ -27,6 +27,26 @@ namespace SFA.DAS.ApiContracts.Build.Tests.ApiResponses
     
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.2.0.0 (NJsonSchema v11.1.0.0 (Newtonsoft.Json v13.0.0.0))")]
+    public partial class DasRequestResponse
+    {
+        [Newtonsoft.Json.JsonProperty("id", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Guid Id { get; set; } = default!;
+
+        [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string? Name { get; set; } = default!;
+
+        private System.Collections.Generic.IDictionary<string, object>? _additionalProperties;
+
+        [Newtonsoft.Json.JsonExtensionData]
+        public System.Collections.Generic.IDictionary<string, object> AdditionalProperties
+        {
+            get { return _additionalProperties ?? (_additionalProperties = new System.Collections.Generic.Dictionary<string, object>()); }
+            set { _additionalProperties = value; }
+        }
+
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.2.0.0 (NJsonSchema v11.1.0.0 (Newtonsoft.Json v13.0.0.0))")]
     public enum DasRequestSortOrder
     {
 
