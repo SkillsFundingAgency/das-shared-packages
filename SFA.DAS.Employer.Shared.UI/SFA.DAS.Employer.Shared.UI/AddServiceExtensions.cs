470a520b6444838c172e4a64eb8a47b23ad65660
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-05-01 13:45:09 +0100

    add service extensions

---
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/AddServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Employer.Shared.UI.Extensions;
+using SFA.DAS.Employer.Shared.UI.Models;
+
+namespace SFA.DAS.Employer.Shared.UI
+{
+    public static class AddServiceExtensions
+    {
+        public static void AddEmployerUiServiceRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var section = configuration.GetSection(nameof(EmployerSharedUIConfiguration));
+            if (!section.GetChildren().Any())
+            {
+                throw new Exception("Cannot find EmployerSharedUIConfiguration in configuration. Please add a section called EmployerSharedUIConfiguration with DashboardUrl property");
+            }
+
+            var config = new EmployerSharedUIConfiguration
+            {
+                DashboardUrl = section[nameof(EmployerSharedUIConfiguration.DashboardUrl)]
+            };
+
+            services.AddSingleton(config);
+            services.AddSingleton<IExternalUrlHelper, ExternalUrlHelper>();
+        }
+    }
+}
