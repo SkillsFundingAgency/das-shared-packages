db7a9bfff2e825d36e239bf3f50c47b850f3d5f1
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-05-01 17:13:35 +0100

    add required package

--- SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/AddServiceExtensions.cs
+++ SFA.DAS.Employer.Shared.UI/SFA.DAS.Employer.Shared.UI/AddServiceExtensions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SFA.DAS.Employer.Shared.UI.Extensions;
 using SFA.DAS.Employer.Shared.UI.Models;
 
@@ -11,18 +12,13 @@ namespace SFA.DAS.Employer.Shared.UI
     {
         public static void AddEmployerUiServiceRegistration(this IServiceCollection services, IConfiguration configuration)
         {
-            var section = configuration.GetSection(nameof(EmployerSharedUIConfiguration));
-            if (!section.GetChildren().Any())
+            if (!configuration.GetSection("EmployerSharedUIConfiguration").GetChildren().Any())
             {
                 throw new Exception("Cannot find EmployerSharedUIConfiguration in configuration. Please add a section called EmployerSharedUIConfiguration with DashboardUrl property");
             }
 
-            var config = new EmployerSharedUIConfiguration
-            {
-                DashboardUrl = section[nameof(EmployerSharedUIConfiguration.DashboardUrl)]
-            };
-
-            services.AddSingleton(config);
+            services.Configure<EmployerSharedUIConfiguration>(configuration.GetSection("EmployerSharedUIConfiguration"));
+            services.AddSingleton((IServiceProvider cfg) => cfg.GetService<IOptions<EmployerSharedUIConfiguration>>().Value);
             services.AddSingleton<IExternalUrlHelper, ExternalUrlHelper>();
         }
     }
